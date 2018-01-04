b1c652b063bf8ec576a2d37abb44194afb4cccb2
phpprjonein <phpprjonein@gmail.com>
2018-01-04 22:43:08 +0530

    discovery table market check updates

--- functions.php
+++ functions.php
@@ -1564,11 +1564,11 @@ function insert_ip_allocation($values){
 }
 function load_discovery_dataset($class = 'C'){
     global $db2;
-    $sqlm = "select GROUP_CONCAT(market) as market from nodes where csr_site_tech_id like '".$_SESSION['username']."'";
+    $sqlm = "select GROUP_CONCAT(DISTINCT(CONCAT('''', (market), '''' ))) as market from nodes where csr_site_tech_id like '".$_SESSION['username']."'";
     $db2->query($sqlm);
     $mrecordset = $db2->resultset();
     $market = $mrecordset[0]['market'];
-    $sql = "SELECT * FROM discoveryres where class ='".strtolower($class)."' AND market in ('".$market."') ORDER BY id";
+    $sql = "SELECT * FROM discoveryres where class ='".strtolower($class)."' AND market in (".$market.") ORDER BY id";
     $db2->query($sql);
     $resultset['result'] = $db2->resultset();
     return $resultset;

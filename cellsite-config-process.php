7c9c725ad4af9187cce42aecad04a59838a4258a
phpprjonein <phpprjonein@gmail.com>
2018-01-10 00:01:21 +0530

    DB insert perf improvement

--- cellsite-config-process.php
+++ cellsite-config-process.php
@@ -6,14 +6,24 @@ $userid = $_SESSION['userid'];
 if($_POST['action'] == 'SaveDB'){
     $db2 = new db2();
     $templname = 'templ_'.generateRandomString();
+    $sql = 'INSERT INTO `configtemplate` (`templname`, `elemid`, `elemvalue`, `editable`) VALUES';
+    $oc = 0;
     foreach ($_POST['loop'] as $key => $val){
+        $oc++;$inc = 0;
         foreach($val as $linekey => $lineval){
-            $sql = "INSERT INTO `configtemplate` (`templname`, `elemid`, `elemvalue`, `editable`) VALUES('".$templname."','".str_replace('looper_','',$key).$linekey."','".$lineval."','".$_POST['hidden'][$key][$linekey]."')";
-            $db2->query($sql);
-            $db2->execute();
+            $inc++;
+            if(count($_POST['loop']) == $oc && count($lineval) == $inc){
+                $sql .= "('".$templname."','".str_replace('looper_','',$key).$linekey."','".$lineval."','".$_POST['hidden'][$key][$linekey]."')";
+            }else{
+                $sql .= "('".$templname."','".str_replace('looper_','',$key).$linekey."','".$lineval."','".$_POST['hidden'][$key][$linekey]."'),";
+            }
         }
     }
-    $_SESSION['msg'] = 'dbs';
+    if($inc > 0){
+        $db2->query($sql);
+        $db2->execute();
+        $_SESSION['msg'] = 'dbs';
+    }
     header("location:cellsitetech-configuration.php");
 }elseif ($_POST['action'] == 'Saveasscriptfile'){
     $file = fopen(getcwd()."/upload/sampleconfigfile.script","w");

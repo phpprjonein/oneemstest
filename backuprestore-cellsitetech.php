d8f6619746611d9e595bc8e3c94cab8c8f0474b7
phpprjonein <phpprjonein@gmail.com>
2018-01-15 23:43:40 -0500

    BackupandRestorepages

---
+++ backuprestore-cellsitetech.php
@@ -0,0 +1,49 @@
+
+<?php
+include "classes/db2.class.php";
+include "classes/paginator.class.php";  
+include 'functions.php'; 
+?>
+    <?php  
+                                               //  Python API Request using curl Begins                    
+            $userid = $_GET['userid'];
+            $deviceid = $_GET['deviceid'];               
+            $devicetype='ios';
+            $url_send =" http://txaroemsda2z.nss.vzwnet.com:8080/healthcheck/";
+            $deviceid = $_GET['deviceid'];              
+ 	          $url_final = 'http://txaroemsda2z.nss.vzwnet.com:8080/healthcheck/'.$devicetype.'/'.$deviceid;			
+            $output = json_decode(sendPostData($url_final),true);   
+            $_SESSION['deviceidcs'] = $deviceid;                 
+     ?>
+
+                <div class="ownfont box-body launch-modal">
+                  <table class="table table-bordered"  cellspacing="0" cellpadding="0" >				  
+                    <tbody>
+                        <tr>                  
+							<td><b><?php echo 'Backup file name one';?><b></td>                                              
+                            <td><b><?php echo date('Y-m-d H:i:s');?><b></td>
+                            <td><b><?php echo 'Manual'?><b><b></td>
+                             <td><b><!-- Button trigger modal -->
+							 <!--
+								<button type="button" class="btn btn-primary anchorcmd" data-toggle="modal" data-target="#mycmdModal">Restore
+								</button>
+								-->
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal" data-remote="remote-page.html">
+								Open modal </button>
+								</td>
+                         </tr>
+						<tr>                  
+							<td><b><?php echo 'Backup file name two';?><b></td>                                              
+                            <td><b><?php echo date('Y-m-d H:i:s');?><b></td>
+                            <td><b><?php echo 'Automatic'?><b><b></td>
+                             <td><b><!-- Button trigger modal -->
+								<!--
+								<button type="button" class="btn btn-primary anchorcmd" data-toggle="modal" data-target="#mycmdModal">Restore
+								</button>-->
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+								Open modal </button>
+								</td>
+                         </tr>						 
+                    </tbody>
+                                       </table>
+                                    </div> 
---
+++ devdetmdl-cellsitebackuprestore.php
@@ -0,0 +1,30 @@
+<?php 
+        
+        $deviceid = $_GET['deviceid'];
+        $url='http://63.49.0.192:8080/backuprestore/ios/'.$deviceid;
+?>
+         
+<div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLabel"> Back up and Restore  - <?php echo $_GET['deviceid'];?></h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+
+<div class="modal-body">    
+<?php
+        if (isset($url)) {
+           if (!$data = @file_get_contents($url)) {
+             $error = error_get_last(); 
+             $data = "HTTP request failed. Error was: " . $error['message'];
+           } 
+        } else 
+            $data = "CLI command needs is in progress.";
+          
+     echo "<span style='font-family:courier;font-size:12px;font-weight:500;'> $data</span>";
+?>  
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    
+</div>

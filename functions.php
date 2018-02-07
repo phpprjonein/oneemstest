8d196d7d3f9f290bc370869c8d498af2b7ff59b5
phpprjonein <phpprjonein@gmail.com>
2018-02-07 09:24:46 +0530

    ip start end address calculation changed

--- functions.php
+++ functions.php
@@ -1579,16 +1579,147 @@ function getipvfour_details($snm)
 }
 */
 function getipvfour_details($range){
-    $parts = explode('/',$range);
-    $ip_exp_add_arr = explode('.',$parts[0]);
-    $ip_exp_add_arr[count($ip_exp_add_arr) - 1] = 0;
-    $parts[0] = implode('.',$ip_exp_add_arr);
-    $exponent = 32-$parts[1].'-';
-    $count = pow(2,$exponent);
-    $start = ip2long($parts[0]) + 1;
-    $end = $start+($count-2);
-    return array_map('long2ip', range($start, ($end-1)) );
+    $range=rtrim($range);
+    if (ereg("/",$range)){  //if cidr type mask
+        $dq_host = strtok("$range", "/");
+        $cdr_nmask = strtok("/");
+        if (!($cdr_nmask >= 0 && $cdr_nmask <= 32)){
+            tr("Invalid CIDR value. Try an integer 0 - 32.");
+            print "$end";
+            exit ;
+        }
+        $bin_nmask=cdrtobin($cdr_nmask);
+        $bin_wmask=binnmtowm($bin_nmask);
+    } else { //Dotted quad mask?
+        $dqs=explode(" ", $range);
+        $dq_host=$dqs[0];
+        $bin_nmask=dqtobin($dqs[1]);
+        $bin_wmask=binnmtowm($bin_nmask);
+        if (ereg("0",rtrim($bin_nmask, "0"))) {  //Wildcard mask then? hmm?
+            $bin_wmask=dqtobin($dqs[1]);
+            $bin_nmask=binwmtonm($bin_wmask);
+            if (ereg("0",rtrim($bin_nmask, "0"))){ //If it's not wcard, whussup?
+                print("Invalid Netmask.");
+                print "$end";
+                exit ;
+            }
+        }
+        $cdr_nmask=bintocdr($bin_nmask);
+    }
+    
+    //Check for valid $dq_host
+    if(! ereg('^0.',$dq_host)){
+        foreach( explode(".",$dq_host) as $octet ){
+            if($octet > 255){
+                print("Invalid IP Address");
+                print $end ;
+                exit;
+            }
+            
+        }
+    }
+    
+    $bin_host=dqtobin($dq_host);
+    $bin_bcast=(str_pad(substr($bin_host,0,$cdr_nmask),32,1));
+    $bin_net=(str_pad(substr($bin_host,0,$cdr_nmask),32,0));
+    $bin_first=(str_pad(substr($bin_net,0,31),32,1));
+    $bin_last=(str_pad(substr($bin_bcast,0,31),32,0));
+    $host_total=(bindec(str_pad("",(32-$cdr_nmask),1)) - 1);
+    
+    if ($host_total <= 0){  //Takes care of 31 and 32 bit masks.
+        $bin_first="N/A" ; $bin_last="N/A" ; $host_total="N/A";
+        if ($bin_net === $bin_bcast) $bin_bcast="N/A";
+    }
+    
+    //Determine Class
+    if (ereg('^0',$bin_net)){
+        $class="A";
+        $dotbin_net= "<font color=\"Green\">0</font>" . substr(dotbin($bin_net,$cdr_nmask),1) ;
+    }elseif (ereg('^10',$bin_net)){
+        $class="B";
+        $dotbin_net= "<font color=\"Green\">10</font>" . substr(dotbin($bin_net,$cdr_nmask),2) ;
+    }elseif (ereg('^110',$bin_net)){
+        $class="C";
+        $dotbin_net= "<font color=\"Green\">110</font>" . substr(dotbin($bin_net,$cdr_nmask),3) ;
+    }elseif (ereg('^1110',$bin_net)){
+        $class="D";
+        $dotbin_net= "<font color=\"Green\">1110</font>" . substr(dotbin($bin_net,$cdr_nmask),4) ;
+        $special="<font color=\"Green\">Class D = Multicast Address Space.</font>";
+    }else{
+        $class="E";
+        $dotbin_net= "<font color=\"Green\">1111</font>" . substr(dotbin($bin_net,$cdr_nmask),4) ;
+        $special="<font color=\"Green\">Class E = Experimental Address Space.</font>";
+    }
+    
+    if (ereg('^(00001010)|(101011000001)|(1100000010101000)',$bin_net)){
+        $special='<a href="http://www.ietf.org/rfc/rfc1918.txt">( RFC-1918 Private Internet Address. )</a>';
+    }
+    
+    return array_map('long2ip', range(ip2long(bintodq($bin_first)), ip2long(bintodq($bin_last))));
 }
+
+
+function binnmtowm($binin){
+    $binin=rtrim($binin, "0");
+    if (!ereg("0",$binin) ){
+        return str_pad(str_replace("1","0",$binin), 32, "1");
+    } else return "1010101010101010101010101010101010101010";
+}
+
+function bintocdr ($binin){
+    return strlen(rtrim($binin,"0"));
+}
+
+function bintodq ($binin) {
+    if ($binin=="N/A") return $binin;
+    $binin=explode(".", chunk_split($binin,8,"."));
+    for ($i=0; $i<4 ; $i++) {
+        $dq[$i]=bindec($binin[$i]);
+    }
+    return implode(".",$dq) ;
+}
+
+function bintoint ($binin){
+    return bindec($binin);
+}
+
+function binwmtonm($binin){
+    $binin=rtrim($binin, "1");
+    if (!ereg("1",$binin)){
+        return str_pad(str_replace("0","1",$binin), 32, "0");
+    } else return "1010101010101010101010101010101010101010";
+}
+
+function cdrtobin ($cdrin){
+    return str_pad(str_pad("", $cdrin, "1"), 32, "0");
+}
+
+function dotbin($binin,$cdr_nmask){
+    // splits 32 bit bin into dotted bin octets
+    if ($binin=="N/A") return $binin;
+    $oct=rtrim(chunk_split($binin,8,"."),".");
+    if ($cdr_nmask > 0){
+        $offset=sprintf("%u",$cdr_nmask/8) + $cdr_nmask ;
+        return substr($oct,0,$offset ) . "&nbsp;&nbsp;&nbsp;" . substr($oct,$offset) ;
+    } else {
+        return $oct;
+    }
+}
+
+function dqtobin($dqin) {
+    $dq = explode(".",$dqin);
+    for ($i=0; $i<4 ; $i++) {
+        $bin[$i]=str_pad(decbin($dq[$i]), 8, "0", STR_PAD_LEFT);
+    }
+    return implode("",$bin);
+}
+
+function inttobin ($intin) {
+    return str_pad(decbin($intin), 32, "0", STR_PAD_LEFT);
+}
+
+
+
 function getipvsix_details($range){
     // Split in address and prefix length
     list($firstaddrstr, $prefixlen) = explode('/', $range);

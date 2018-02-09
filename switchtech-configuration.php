a6cd67345824911fc30419ec407e53bce802701a
phpprjonein <phpprjonein@gmail.com>
2018-02-09 08:36:03 +0530

    config page default template file added and made readonly as more readable

--- switchtech-configuration.php
+++ switchtech-configuration.php
@@ -81,6 +81,9 @@ $page_title = 'OneEMS';
 										<div class="col-lg-8 tags p-b-2">
 											<?php
 							$filename = getcwd()."/upload/sampleconfigfile_".$_SESSION['userid'].".txt";
+							if(!file_exists($filename)){
+							    $filename = getcwd()."/upload/Default_Gold_ASR920_Great-Lakes_Allnew.txt";
+							}
 							$output = '<form name="file_process" action="cellsite-config-process.php" method="post" class="border">';
 							$output .= '<div class="form-group cb-control"><label>Hide Readonly Fields&nbsp;</label><input type="checkbox" value="1" id="show_hide_readonly"/></div>';
 							?>

e1304507038a897d9c1ef166b15cb44a004836da
phpprjonein <phpprjonein@gmail.com>
2018-02-23 09:22:46 +0530

    manual discovery api updates

--- api-test-manual-device-disc.php
+++ api-test-manual-device-disc.php
@@ -1,6 +1,7 @@
 <?php
-/* Sample json output 
-{
+//$url_final = 'http://63.49.0.192:8080/snmp/10.198.238.2';
+//$output = json_decode(sendPostData($url_final),true);
+$output = json_decode('{
   "data": {
     "devicename": "",
     "deviceos": "",
@@ -16,64 +17,60 @@
     "upsince": ""
   },
   "deviceIpAddr": "10.198.238.19"
-} 
-
-end */
-/*
-if(rand(0,1)){
-    $ret = true;
-}else{
-    $ret = false;
-}
-$myObject = array( 'result' => $ret ); // example
-echo json_encode($myObject); // outputs JSON text
-*/
-
-/*
-       $deviceid = $_POST['deviceid'];               
-       $url_send = 'http://63.49.0.192:8084/configbackup/'.$deviceid;
-        $json_response = sendPostData($url_send);
-*/
-// An associative array
-$json = '{
-  "data": {
-    "devicename": "",
-    "deviceos": "",
-    "deviceseries": "",
-    "discoverystatus": "Failed",
-    "lastpolled": "2018-02-21 21:56",
-    "model": "",
-    "nodeVersion": "",
-    "nodestatus": "C",
-    "status": false,
-    "sys_contact": "",
-    "sys_location": "",
-    "upsince": ""
-  },
-  "deviceIpAddr": "10.198.238.19"}';
-$output = json_decode($json,true);
-echo  "<b> Device Ip Address : </b>".$output['deviceIpAddr']."<br>";
-echo "<br>";
-echo "<b>Device Name       : ".$output['data']['devicename']."<br>";
-echo "<b>Device OS         : ".$output['data']['deviceos']."<br>";
-echo "<b>Device Series     : ".$output['data']['deviceseries']."<br>";
-echo "<b>Discovery Status  : ".$output['data']['discoverystatus']."<br>";
-echo "<b>Last Polled       : ".$output['data']['lastpolled']."<br>";
-echo "<b>Model             : ".$output['data']['model']."<br>";
-echo "<b>Node Version      : ".$output['data']['nodeVersion']."<br>";
-echo "<b>Node Status       : ".$output['data']['nodestatus']."<br>";
-echo "<b>System Contact    : ".$output['data']['sys_contact']."<br>";
-echo "<b>System Location   : ".$output['data']['sys_location']."<br>";
-echo "<br>";;
-
-/*
-$deviceid = $_POST['deviceid'];               
-$url_send = 'http://63.49.0.192:8084/configbackup/'.$deviceid;
-$json_response = sendPostData($url_send);
-$output = json_decode($json_response,true);   
-echo "<br>";
-echo "<b>Backup Status : ".$output['data'][0]['status']."</b>";
-echo "<br/>";
-echo "<b>Filename : ".$output['data'][0]['data']."</b>";
-*/
+}',1);
 ?>
+<div class="container">
+  <p><b>Device Ip Address : <?php echo $output['deviceIpAddr']; ?></b></p>            
+  <table class="table table-striped">
+    <tbody>
+      <tr>
+        <td><h6>Device Name</h6></td>
+        <td><?php echo $output['data']['devicename']; ?></td>
+      </tr>
+      <tr>
+        <td>Device OS</td>
+        <td><?php echo $output['data']['deviceos']; ?></td>
+      </tr>
+      <tr>
+        <td>Device Series</td>
+        <td><?php echo $output['data']['deviceseries']; ?></td>
+      </tr>
+      <tr>
+        <td>Discovery Status</td>
+        <td><?php echo $output['data']['discoverystatus']; ?></td>
+      </tr>
+      <tr>
+        <td>Last Polled</td>
+        <td><?php echo $output['data']['lastpolled']; ?></td>
+      </tr>
+      <tr>
+        <td>Model</td>
+        <td><?php echo $output['data']['model']; ?></td>
+      </tr>
+      <tr>
+        <td>Node Version</td>
+        <td><?php echo $output['data']['nodeVersion']; ?></td>
+      </tr>
+      <tr>
+        <td>Node Status</td>
+        <td><?php echo $output['data']['nodestatus']; ?></td>
+      </tr>
+      <tr>
+        <td>Status</td>
+        <td><?php echo $output['data']['status']; ?></td>
+      </tr>
+      <tr>
+        <td>System Contact</td>
+        <td><?php echo $output['data']['sys_contact']; ?></td>
+      </tr>
+      <tr>
+        <td>System Location</td>
+        <td><?php echo $output['data']['sys_location']; ?></td>
+      </tr>
+      <tr>
+        <td>Up Since</td>
+        <td><?php echo $output['data']['upsince']; ?></td>
+      </tr>
+    </tbody>
+  </table>
+</div>

bf5c7d78c6e507f1f736e60ec2c3d9780af8432f
phpprjonein <phpprjonein@gmail.com>
2018-03-18 01:15:30 +0530

    script page updates as per new flow

--- new1.php
+++ new1.php
@@ -1,43 +1,11 @@
-<?php
-include "classes/db2.class.php";
-include "classes/paginator.class.php";
-include 'functions.php';
-
-// Static variable values set
-if (isset($_GET['clear'])) {
-    if (strtolower($_GET['clear']) == 'search') {
-        unset($_SESSION['search_term']);
-    }
-}
-
-user_session_check();
-check_user_authentication('1'); // cellsite tech type user
-
-$page_title = 'OneEMS';
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
-   <?php //include("includes.php");  ?>
+<title>Test New 123</title>
 </head>
 <body>
-	<?php //include ('menu.php'); 
-        unset($_SESSION['filename']);  ?> 
-	<form action="cellsitetech-configuration.php" method="post" id="config_file_uploader">
-		<div id="status" style="display: none;" class="alert"></div>
+        <form name="file_process" action="help.php" method="post" class="border">
                <input type="submit" name="act" class="btn config-submit1" value="NEXT">
-                       <div class="row">
-          <div class="col" id="template_info">
-            <label for="inputRegion">TEMPLATE:testfile2</label>
-            <small><b><span id="filename"></span></b></small>
-            <input type="hidden" name="filename" value="testfile2" id="upload_filename">
-          </div>
-        </div>
 	</form>
-
-	<!-- container-fluid -->
-
-        <?php //include ('footer.php'); ?> 
     </body>
 </html>

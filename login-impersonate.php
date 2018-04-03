5ecd5ad6f9a9499f4a1818587e21704121ff086b
phpprjonein <phpprjonein@gmail.com>
2018-04-03 07:56:55 +0530

    updated login impersonate page

--- login-impersonate.php
+++ login-impersonate.php
@@ -29,18 +29,13 @@ check_user_authentication(array(8)); //cellsite tech type user
             <?php include ('menu.php'); ?> 
           <div class="content container-fluid" id="login-impersonate">
           			<form class="login-form" action="index.php" method="post">
-          			<input type="hidden" value="imp" name="userimp"/>
-          	        <div class="row">
-                    <div class="form-group col-md-12">
-                      <label for="inputName">User Name</label>
-                      <input type="inputName" name="username" class="form-control-plaintext" id="inputName" placeholder="User Name">
-                    </div>
-                    <div class="form-check col-md-12">
-                      <div class="btn-group" role="group" aria-label="">
-                        <button type="submit" class="btn" id="manual-discovery" data-toggle="modal">Submit</button>
-                      </div>
-                    </div>
-                    </div>
+              			<input type="hidden" value="imp" name="userimp"/>
+              			<div class="input-group" >
+                            <div class="form-inline">
+                              <input type="inputName" name="username" class="form-control" id="inputName" placeholder="User Name">
+                              <button type="submit" class="btn" id="manual-discovery" data-toggle="modal">Submit</button>
+                            </div>
+    					</div>
                     </form>
           			</div>
   		</div>

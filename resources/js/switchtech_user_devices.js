2e597a229c0adad8de8fa045e2c37626cf87e3ef
phpprjonein <phpprjonein@gmail.com>
2018-04-13 09:00:20 +0530

    Backup page - Switch tech user device backup info not loaded fix

--- resources/js/switchtech_user_devices.js
+++ resources/js/switchtech_user_devices.js
@@ -335,56 +335,61 @@ $(document).ready(function() {
 	  		
 	  	  
 	  		$(document).on('click', '#backuprestore tbody td.details-control', function(event) {
-	  	        
-	  	         var tr = $(this).closest('tr');
-	  	         var row = table.row( tr ); 
-	  	         var current_click_row = row.child.isShown();
+				//alert(' reach here');
+	 	       //  Temporarily commented on 31 jan 2018. Needed in future.
+	     		var table =  $('#backuprestore').DataTable();
+	          var tr = $(this).closest('tr');
+	          var row = table.row( tr ); 
+	          var current_click_row = row.child.isShown();
 
-	  	         $('tr.device_row').each(function(){  
-	  	          
-	  	          var orow = table.row(this);
-	  	          // alert(orow.child.isShown());
-	  	          if (orow.child.isShown()){                
-	  	              // This row is already open - close it
-	  	                orow.child.hide();
-	  	                $(this).removeClass('shown');
-	  	          }
-	  	         }); 
+	          $('tr.device_row').each(function(){  
+	           
+	           var orow = table.row(this);
+	           // alert(orow.child.isShown());
+	           if (orow.child.isShown()){                
+	               // This row is already open - close it
+	                 orow.child.hide();
+	                 $(this).removeClass('shown');
+	           }
+	          }); 
 
-	  	         
-	  	          if ( row.child.isShown() ) {
-	  	              // This row is already open - close it
-	  	              row.child.hide();
-	  	              tr.removeClass();
-	  	          }
-	  	          else {
-	  	                  // Open this row
-	  	                  // alert(tr.attr('id'));
-	  	                  if (!current_click_row ){
-	  	                  row.child( format(tr.attr('id')) ).show();
-	  	                  tr.addClass('shown');
+	          
+	           if ( row.child.isShown() ) {
+	               // This row is already open - close it
+	               row.child.hide();
+	               tr.removeClass();
+	           }
+	           else {
+	                   // Open this row
+	                   // alert(tr.attr('id'));
+	                   if (!current_click_row ){
+	                   row.child( format(tr.attr('id')) ).show();
+	                   tr.addClass('shown');
 
-	  	                  var id = tr.attr('id').replace('row_','');  
-	  	                  var ajs = $.ajax({
-	  	                    type:"get",
-	  	                    url:"backuprestore-cellsitetech.php",					
-	  	                  data: {'deviceid':id, 'userid':$('#userid').val(), 'devicename': $(this).closest('tr').find("td:eq(1)").text(), 'region': $(this).closest('tr').find("td:eq(4)").text(), 'market' : $(this).closest('tr').find("td:eq(5)").text()},
-	  	                    beforeSend: function(){
-	  	                        $('#detail_'+id).html('<div class="text-center overlay box-body">Running Health Checks. Takes several minutes... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>');
-	  	                    },
-	  	                    complete: function() {
-	  	                        $('#detail_'+id).addClass('loaded');
-	  	                    },
-	  	                    success: function(resdata){
-	  	                        $('#detail_'+id).html(resdata);
-	  	                    }
-	  	                });
-	  	            }
-	  	          }
+	                   var id = tr.attr('id').replace('row_','');  
+	                   var ajs = $.ajax({
+	                     type:"get",
+	                     url:"backuprestore-cellsitetech.php",					
+	                     data: {'deviceid':id, 'userid':$('#userid').val(), 'devicename': $(this).closest('tr').find("td:eq(1)").text(), 'region': $(this).closest('tr').find("td:eq(4)").text(), 'market' : $(this).closest('tr').find("td:eq(5)").text()},
+	                     beforeSend: function(){
+	                         $('#detail_'+id).html('<div class="text-center overlay box-body">Loading... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>');
+	                     },
+	                     complete: function() {
+	                         $('#detail_'+id).addClass('loaded');
+	                     },
+	                     success: function(resdata){
+	                         $('#detail_'+id).html(resdata);
+	                     }
+	                 });
+	                   
+	                   
+	                   
+	             }
+	           }
 
-	  	             
+	             
 
-	  	      });	
+	       });	
 });
 function copyToClipboard(element) {
 	  var $temp = $("<input>");

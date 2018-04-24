34e67a371488f80fabe2d7ddf4bb708e5bc797f9
phpprjonein <phpprjonein@gmail.com>
2018-04-24 23:53:57 +0530

    updated batch with accordion batch details

---
+++ batchtrack-load-table-data.php
@@ -0,0 +1,24 @@
+<?php
+include "classes/db2.class.php";
+include "classes/paginator.class.php";  
+include 'functions.php';
+$deviceid = $_POST['deviceid'];
+$deviceinfo = generic_get_node_info_by_deviceid($deviceid);
+?>                
+                
+                <div class="ownfont box-body launch-modal">
+                 <table class="table table-bordered"  cellspacing="0" cellpadding="0" >
+                    <tbody>
+                                            <tr>
+                                              <td><b>IP Address</b></td>
+                                              <td><b>Sys Name</b></td>
+                                              <td><b>Execution Status</b></td>
+                                            </tr>
+                                            <tr>
+                                              <td><?php echo $deviceinfo['result'][0]['deviceIpAddr'].$deviceinfo['result'][0]['deviceIpAddrsix'];?></td>
+                                              <td><?php echo $deviceinfo['result'][0]['systemname']; ?></td>
+                                              <td><?php echo $_POST['exestatus']; ?></td>
+                                            </tr>
+					</tbody>
+				</table>
+				</div>

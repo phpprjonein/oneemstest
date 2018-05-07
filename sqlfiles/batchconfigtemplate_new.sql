98ea7dd6bf8a092b491cc037f8f5899ee2af4696
phpprjonein <phpprjonein@gmail.com>
2018-05-06 23:44:41 -0400

    modified devbatch,devmstfiles 06MAY2018

---
+++ sqlfiles/batchconfigtemplate_new.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2018 at 05:43 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `oneems`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `batchconfigtemplate_new`
+--
+
+CREATE TABLE IF NOT EXISTS `batchconfigtemplate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `batchid` int(11) NOT NULL,
+  `scriptlist` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

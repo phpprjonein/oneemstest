d67ce69d13b6d5deb11d92545917fa984211766a
phpprjonein <phpprjonein@gmail.com>
2018-05-08 17:34:15 -0400

    tablemodified

--- sqlfiles/batchmaster.sql
+++ sqlfiles/batchmaster.sql
@@ -1,5 +1,29 @@
----
--- Table structure for table `devbatchmst_new`
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2018 at 11:26 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `oneems`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `batchmaster`
 --
 
 CREATE TABLE IF NOT EXISTS `batchmaster` (
@@ -18,3 +42,7 @@ CREATE TABLE IF NOT EXISTS `batchmaster` (
   `refmop` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

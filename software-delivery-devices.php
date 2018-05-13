73d39c3edbfb98d1534a9452400db26a99e63ba7
phpprjonein <phpprjonein@gmail.com>
2018-05-13 09:56:32 -0400

    Design changes

--- software-delivery-devices.php
+++ software-delivery-devices.php
@@ -1,94 +1,70 @@
 <?php
-ini_set('display_errors',1);
 include_once "classes/db2.class.php";
 include_once "classes/paginator.class.php";
-function swrepo_get_deviceseries() {
-    global $db2;
-    $sql = "SELECT distinct(deviceseries) FROM swrepository order by deviceseries asc";
+ function swrepo_get_deviceseries() {
+	  global $db2;
+    $sql = "SELECT * FROM swrepository";
     $db2->query($sql);
     $resultset = $db2->resultset();
     return $resultset;
-}
-function swrepo_get_nodeversions() {
-    global $db2;
-    $sql = "SELECT distinct(nodeVersion) FROM swrepository order by nodeVersion asc";
+ };	
+ function swrepo_get_nodeversions() {
+	  global $db2;
+    $sql = "SELECT * FROM swrepository";
     $db2->query($sql);
     $resultset = $db2->resultset();
-    return $resultset;
-}
+    return $resultset;	
+};
+
 function swrepo_get_filenames() {
-    global $db2;
-    $sql = "SELECT distinct(filename) FROM swrepository order by filename asc";
+	  global $db2;
+    $sql = "SELECT * FROM swrepository";
     $db2->query($sql);
     $resultset = $db2->resultset();
-    return $resultset;
-}
-?>
+    return $resultset;	
+}; 
+ ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Software Delivery</title>
- <?php //include_once("includes.php")?>
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="https://code.jquery.com/jquery-1.12.4.js"> </script>
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"> </script>
-
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"> </script>
+  <title>One Ems</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  
+  
+<!--  <script src="https://code.jquery.com/jquery-1.12.4.js"> </script> -->
+  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"> </script>
 <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"> </script>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css">
+<!--<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css">-->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"> 
 <style>
-* {
-    box-sizing: border-box;
-}
-
-/* Create two equal columns that floats next to each other */
-.column {
-    float: left;
-    width: 50%;
-    padding: 10px;
-  /*  height: 300px; */ /* Should be removed. Only for demonstration */
-}
-
-/* Clear floats after the columns */
-.row:after {
-    content: "";
-    display: table;
-    clear: both;
-}
-td {word-wrap: break-word;word-break: break-all;} 
-.dt-buttons button{width:2.5rem;height:2rem;border:none !important}
-.dt-buttons span{display:none}
-
-.dtexcelbtn{background:url(resources/img/xlsx.jpg) no-repeat center right !important}.dtpdfbtn{background:url(resources/img/pdf.jpg) no-repeat center right !important}.dtprintbtn{background:url(resources/img/print.png) no-repeat center right !important}div.dt-buttons{float:right}
-.dt-buttons button{text-content:.;}
 </style>
-<script >
+  <script >
     
 $(document).ready(function () {
         $('#swdelvrybatchpro').DataTable({
             "processing": true,
             "serverSide": true,
 			 "dom": 'Bfrtip',
-			 "buttons": [{extend: 'excelHtml5',text: '', titleAttr:'Excel',className:'dtexcelbtn'},
-				 {extend: 'pdfHtml5',text: '',titleAttr:'PDF',className:'dtpdfbtn'},
-				 {extend: 'copyHtml5',text: '',titleAttr:'Copy',className:'dtprintbtn'}
-				 ],
+        "buttons": [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ],
             "ajax": {
                 url: 'software-delivery-process.php',
                 type: 'POST'
             },
-            "columns": [ 		
-                {  "className":      'batch-control',
-                    "orderable":      false,
-                    "data":           null,
-                    "defaultContent": "<input type='checkbox' id = 'batchchkbox' class='btn btn-primary' data-toggle='modal'>"},	 
+            "columns": [ 			 
                 {"data": "id"},
                 {"data": "deviceIpAddr"},
                 {"data": "systemname"},
@@ -97,6 +73,7 @@ $(document).ready(function () {
                 {"data": "nodeVersion"}
             ],
 			"order": [[4, 'asc']]
+			
         });
     });	
 	/*
@@ -120,29 +97,7 @@ $(document).ready(function () {
 	$('#batchModal').on('hidden.bs.modal', function () {
     		 location.reload();
     	}) 
-		
-function get_nodeversion(val){
-    $.ajax({
-      type: "POST",
-      url: 'getnodeversion.php',
-      data: ({deviceseries:val}),
-      success: function(data) {
-        alert(data);
-      }
-    });
-  };
-		
-
-function get_swrepofiles(val){
-    $.ajax({
-      type: "POST",
-      url: 'getswrepofiles.php',
-      //data: ({deviceseries:val}),
-      success: function(data) {
-        alert(data);
-      }
-    });
-  };
+ 
 */		
  $(document).ready(function(){
             $('#device_series').change(function(){
@@ -182,12 +137,7 @@ $(document).ready(function() {
 
     // process the form
 	 $('form').submit(function(event) {  
-    //$('form').submit(function(event) { title = $("#create-item").find("input[name='node_version']").val();
-          //alert('value of device series is' + $('input[name=device_series]').val());
-		  //alert('value of device series is' + $('device_series').val());
-		  //alert('value of node version is' + $('input[name=node_version]').val());
-		  //alert('value of node version is' + $('#form').find("input[name='node_version']").val());
-		  alert('value of node version is' + $('#node_version').val());
+        alert('value of node version is' + $('#node_version').val());
         // get the form data
         // there are many ways to get this data using jQuery (you can use the class or id also)
        /* var formData = {
@@ -231,7 +181,6 @@ $(document).ready(function() {
 </script>
 </head>
 <body>
-<div class="container-fluid">
 <!-- The Modal -->
 		<div class="modal fade" id="batchModal">
 		  <div class="modal-dialog" >
@@ -255,41 +204,69 @@ $(document).ready(function() {
 			</div>
 		  </div>
 		</div>
-		<div class="col-md-12">
-<h2>Software Delivery</h2>
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1> Software Delivery</h1>
+</div>
 
-<div class="row">
-<div class="column col-sm-4" style="background-color:#aaa;">
-<!-- <form action="software/html/tags/html_form_tag_action.cfm"> -->
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Network Elements</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Backup</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Configuration</a>
+      </li>    
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Discovery</a>
+      </li>    
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Maintenance</a>
+      </li>    
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Help</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+<div class="container" style="margin-top:30px">
 <form action="software-delivery-batch-process.php" method="POST">
-<?php $swrepods = swrepo_get_deviceseries();?>
+  <div class="row">
+    <div class="col-lg-4">
+ <!--<form action="software/html/tags/html_form_tag_action.cfm">-->
+<?php $swrepolist = swrepo_get_deviceseries();// print_r($swrepolist); ?>
+
 <fieldset class="form-group">
 <label for="device_series">Device Series</label>
 <select class="custom-select" id ="device_series" name ="device_series"> 
 <option selected>Choose Device Series</option>
-<?php foreach ($swrepods as $key=>$val):?>
-	<option><?php echo $val['deviceseries']; ?></option>
-<?php endforeach;?>
+<?php print_r($swrepolist);foreach ($swrepolist as $key => $val){ ?>
+<option value="<?php echo $val['deviceseries'];?>"><?php echo $val['deviceseries'];?></option>
+<?php }; ?>
 </select>
 </fieldset>
-<?php $swreponv = swrepo_get_nodeversions();?>
+
+<?php $swreponodeversions = swrepo_get_nodeversions();// print_r($swreponodeversions); ?>
+
 <fieldset class="form-group">
 <label for="os_version">Node Version</label>
 <select class="custom-select" id ="node_version" name ="node_version">
 <option selected>Choose OS Version </option>
-<?php foreach ($swreponv as $key=>$val):?>
-	<option><?php echo $val['nodeVersion']; ?></option>
-<?php endforeach;?>
 </select>
 </fieldset>
-<?php $swrepofn = swrepo_get_filenames();?>
+<?php $swrepogetfilenames = swrepo_get_filenames(); //print_r($swrepogetfilenames); ?>
 <fieldset class="form-group">
 <label for="swrp_filename">File Name</label>
 <select class="custom-select" id ="swrp_filename" name ="swrp_filename">
 <option selected>Choose Filename</option>
-<?php foreach ($swrepofn as $key=>$val):?>
-	<option><?php echo $val['filename']; ?></option>nv
-<?php endforeach;?>
 </select>
 </fieldset>
 <fieldset class="form-group">
@@ -308,15 +285,19 @@ $(document).ready(function() {
 </select>
 
 </fieldset>							
- 
 </div>
-  <div class="column col-sm-8" style="background-color:#bbb;">
+    <div class="col-lg-8">
+        <h2> Devices List </h2>
+<p id="cp1" style="display: none"></p>
 <input type="hidden" id='userid' value="<?php echo "testuser";//echo $userid ?>" name="">
 <input type="hidden" value="<?php echo time();?>" name="batchid" id="batchid">
+<!-- <input type="hidden" value="<?php //echo $_SESSION['batch_vars']['templname']; ?>" name="scriptname" id="scriptname" /> -->
+<!-- <input type="hidden" value="<?php //echo $_SESSION['batch_vars']['deviceseries']; ?>" name="deviceseries" id="deviceseries"/> -->
+<!-- <input type="hidden" value="<?php //echo $_SESSION['batch_vars']['deviceos']; ?>" name="deviceos" id="deviceos"/>	 -->
+   
 <table id="swdelvrybatchpro" class="display" style="width:100%">
         <thead>
-            <tr>
-                <th></th> 
+            <tr> 
 			    <th>id</th>
                 <th>deviceIpAddr</th>
                 <th>systemname</th>
@@ -325,13 +306,17 @@ $(document).ready(function() {
                 <th>nodeVersion</th>
             </tr>
         </thead>        
-</table> 
-<button type="submit" value="SUBMIT" class="btn btn-default text-center"  id="batch-submit" name="batch-submit">SUBMIT</button>
+</table> 	
   </div>
+<div class="text-center">
+<button type="submit" value="SUBMIT" class="btn btn-default text-center"  id="batch-submit" name="batch-submit">SUBMIT</button>
 </div>
-
+</div>
 </form>
-</div>
+</div>  
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>© 2018 OneEMS. All rights reserved. All content is confidential and proprietary.</p>
 </div>
 </body>
 </html>

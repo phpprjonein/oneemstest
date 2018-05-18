130b8fd85ee8b32f2a45f1db114c998bd281b294
phpprjonein <phpprjonein@gmail.com>
2018-05-17 23:14:11 -0400

    release1.0 and removing unwanted files

--- batch-tracking-devices-server.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include "classes/db2.class.php"; 
-include 'functions.php';
-$backup_list = get_devicebatch_list_from_devicebatch_datatable(); 
-exit(json_encode($backup_list, 1));
-?>
--- usrfavswitchdel.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include "classes/db2.class.php";
-  include 'functions.php';
-  $userid  = $_POST['userid'];
-  $listid  = $_POST['listid'];
-  $switchid  = $_POST['switchid'];
-  $result = usrfavritelistswdel($userid,$listid,$switchid);
-?>

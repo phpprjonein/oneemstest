5cc162d085215d34a6ee145c18f5b63dbbeb7386
phpprjonein <phpprjonein@gmail.com>
2018-05-24 23:05:31 +0530

    Added support button to jira at the bottom near footer

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
-<!--<footer class="footer main-footer col-md-12">
-  <div class="panel panel-default">
-		<div class="panel-footer">© 2018 OneEMS. All rights reserved. All content is confidential and proprietary.</div>
-  </div>
-</footer>-->
-
+<style>
+div.fixed {position: fixed;bottom: 10px;right: 0;z-index: 9999;}
+div.fixed button{background-color: #286090;border-color: #204d74;color: #ffffff;font-weight: bold;}
+div.fixed button:hover{background-color: #286090;border-color: #204d74;color: #ffffff;font-weight: bold;}
+</style>
+<div class="fixed">
+<a href="jira.php"><button type="button" class="btn">Support</button></a>
+</div>
 <footer class="footer">
   <div class="container">
 		<div class="text-center text-muted">© 2018 OneEMS. All rights reserved. All content is confidential and proprietary.</div>
   </div>
 </footer>
 <script  type="text/javascript" language="javascript" src="resources/js/piwik-tracker.js"></script>
+

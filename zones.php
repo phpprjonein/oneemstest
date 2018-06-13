0c6654d9e56987c343ff142f601fe2ff5302bd44
phpprjonein <phpprjonein@gmail.com>
2018-06-13 23:38:54 +0530

    updated api req files

--- zones.php
+++ zones.php
@@ -6,26 +6,19 @@ ini_set('display_errors',1);
 
 $output = @file_get_contents('http://localhost/oneemstest/'.$_SESSION['username'].'_callout_zone_list.php');
 $resp_result_arr = json_decode($output, 1);
+
 print '<pre>';
-print_r($resp_result_arr['user']['prefs']['calloutzones']);
-
-foreach ($resp_result_arr['user']['prefs']['calloutzones'] as $key=>$val):
-//print $_SESSION['username'].'_callout_zone_'.$val.'_list.json'.'<br/>';
-//echo ('calloutzonesAPI/'.$_SESSION['username'].'_callout_zone_'.$val.'_list.json');
-
-echo ('http://localhost/oneemstest/calloutzonesAPI/'.$_SESSION['username'].'_callout_zone_'.$val.'_list.php');
-
-$output_zones = @file_get_contents(urlencode('http://localhost/oneemstest/calloutzonesAPI/'.$_SESSION['username'].'_callout_zone_'.$val.'_list.php'));
-
-print_r($output_zones);
-
-$output_zones_resp_result_arr = json_decode($output_zones, 1);
-print '<pre>';
-print_r($output_zones_resp_result_arr);
-print '<br><br><br><br><br>';
-endforeach;
-
-die;
-
+if(isset($resp_result_arr['user']['prefs']['calloutzones'])){
+    foreach ($resp_result_arr['user']['prefs']['calloutzones'] as $key=>$val):
+    $val = rawurlencode($val);
+    $output_zones = @file_get_contents(('http://localhost/oneemstest/calloutzonesAPI/'.$_SESSION['username'].'_callout_zone_'.$val.'_list.php'));
+    print_r($output_zones);
+    $output_zones_resp_result_arr = json_decode($output_zones, 1);
+    print '<pre>';
+    print_r($output_zones_resp_result_arr);
+    print '<br><br><br><br><br>';
+    endforeach;
+    die;
+}
 
 ?>

1322932ffe83f516b8aeea5dee36a815e7adda70
phpprjonein <phpprjonein@gmail.com>
2018-06-25 14:38:42 -0400

    configtemplateautopopulatetables

---
+++ sqlfiles/usrvars.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 25, 2018 at 08:21 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `oneems`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usrvars`
+--
+
+CREATE TABLE IF NOT EXISTS `usrvars` (
+  `usrvarid` int(11) NOT NULL AUTO_INCREMENT,
+  `usrvarname` text,
+  `usrvarval` text,
+  `deviceseries` text,
+  `templname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`usrvarid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

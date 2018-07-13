3a7c584ebe6950683b83c12a7820b45ac2fe17fc
phpprjonein <phpprjonein@gmail.com>
2018-07-13 23:02:55 +0530

    Other vars pushed to piwik

--- includes.php
+++ includes.php
@@ -66,6 +66,10 @@
     var u="//piwik.vh.vzwnet.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '77']);
+    _paq.push(['setUserId', '<?php echo $_SESSION['username']; ?>']);
+    _paq.push(['setMarket', '']);
+    _paq.push(['setManager name', '']);
+    _paq.push(['setRole', 'Manager']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();

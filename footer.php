9e6689759406fe680031f2bbd660fdc8fe48aeba
phpprjonein <phpprjonein@gmail.com>
2018-08-22 19:22:19 -0400

    commented the jira link

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
 
 <div class="fixed">
+<!--
 	<a id="feedback-button"
 		href="https://onejira.verizon.com/secure/Dashboard.jspa" class="btn"
 		target="_blank">open ticket</a>
+-->
 </div>
+
 <footer class="footer">
 	<div class="container">
 		<div class="text-center text-muted">© 2018 OneEMS. All rights

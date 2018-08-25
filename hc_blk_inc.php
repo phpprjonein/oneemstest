4cb2a604f1f8cc5c75010334d546ac53ba5d0de3
phpprjonein <phpprjonein@gmail.com>
2018-08-25 18:36:57 -0400

    healthcheck url corrected

---
+++ hc_blk_inc.php
@@ -0,0 +1,349 @@
+
+<div class="ownfont box-body launch-modal" id="health-chk-div-wrap">
+	<table class="table table-bordered" cellspacing="0" cellpadding="0">
+		<tbody>
+			<tr>
+				<td style="width: 10px;"><input type="checkbox" name="iosversion"
+					id="iosversion" value="extract_version_ios"
+					<?php if(in_array('extract_version_ios', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>IOS Version </b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=iosversion&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    //$color = ($output['iosversion']['R'] == 0) ? 'green' : 'red';
+    $color = ($output['iosversion']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['iosversion']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td style="width: 10px;"><input id="cpuutilization" type="checkbox"
+					name="cpuutilization" value="extract_process_cpu"
+					<?php if(in_array('extract_process_cpu', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>CPU Utilization</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=cpuutilization&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['cpuutilization']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['cpuutilization']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td style="width: 10px;"><input type="checkbox" id="freememory"
+					name="freememory" value="extract_memory_statistics"
+					<?php if(in_array('extract_memory_statistics', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>Free Memory</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=freememory&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['freememory']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['freememory']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" id="buffers" name="buffers" value="extract_buffers"
+					<?php if(in_array('extract_buffers', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>Buffers</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=buffers&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['buffers']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['buffers']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="bootstatement" name="bootstatement"
+					value="extract_show_running_config" <?php if(in_array('extract_show_running_config', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>Boot Statement</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=bootstatement&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['bootstatement']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['bootstatement']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="platform" name="platform" value="extract_show_platform"
+					<?php if(in_array('extract_show_platform', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>Platform </b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=platform&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['platform']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['platform']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+			</tr>
+
+			<tr>
+				<td><input type="checkbox" id="environmental" name="environmental"
+					value="extract_alarms" <?php if(in_array('extract_alarms', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>Environmental</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=environment&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['environmental']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['environmental']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="configregister" name="configregister"
+					value="extract_version_config" <?php if(in_array('extract_version_config', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>Config Register</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=configregister&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['configregister']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['configregister']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="interfacecounters"
+					name="interfacecounters" value="extract_count_interfaces"
+					<?php if(in_array('extract_count_interfaces', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>Interface Counters</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=interfacecounters&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['interfacecounters']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['interfacecounters']['count'] . '</span>';
+    echo $display;
+    ?>
+</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" id="twothsndbyteping"
+					name="twothsndbyteping" value="twothsndbyteping"
+					<?php if(in_array('twothsndbyteping', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>2000 Byte Ping</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=twothsndbyteping&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+   // print_r($output['twothsndbyteping']);
+    $color = ($output['twothsndbyteping']['twothsndbyteping']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['twothsndbyteping']['twothsndbyteping']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="bfdsession" name="bfdsession"
+					value="extract_bfd_neighbour" <?php if(in_array('extract_bfd_neighbour', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>BFD Sessions</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=bfdsession&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['bfdsession']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['bfdsession']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="logentries" name="logentries"
+					value="extract_show_logging" <?php if(in_array('extract_show_logging', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>Log Entries</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=logentries&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    if (isset($output['logentries']['buffer_logging']['R'])) {
+        $color = ($output['logentries']['buffer_logging']['R'] == 0) ? 'green' : 'red';
+        $display = "<span style='color:" . $color . "'>" . " Buffer Logging : " . $output['logentries']['buffer_logging']['value'] . '</span>';
+        echo $display;
+    }
+    ?>
+											  <?php
+            
+            if (isset($output['logentries']['trap_logging']['R'])) {
+                $color = ($output['logentries']['trap_logging']['R'] == 0) ? 'green' : 'red';
+                $display = "<span style='color:" . $color . "'>" . "<br> Trap Logging : " . $output['logentries']['trap_logging']['value'] . '</span>';
+                echo $display;
+            }
+            ;
+            ?>
+											  </td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" id="vrfstates" name="vrfstates"
+					value="extract_show_vrf" <?php if(in_array('extract_show_vrf', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>VRF</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=vrfstates&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['vrfstates']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['vrfstates']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="interfacestates"
+					name="interfacestates" value="extract_show_brief"
+					<?php if(in_array('extract_show_brief', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>Interface State</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=interfacestates&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['interfacestates']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['interfacestates']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="xconnect" name="xconnect" value="extract_xconnect_all"
+					<?php if(in_array('extract_xconnect_all', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>Xconnect</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=xconnect&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['xconnect']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['xconnect']['count'] . '</span>';
+    echo $display;
+    ?>
+</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" id="mplsinterfaces" name="mplsinterfaces"
+					value="extract_mlps_interfaces" <?php if(in_array('extract_mlps_interfaces', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>MPLS Interfaces</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=mplsinterfaces&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    $color = ($output['mplsinterfaces']['extract_mpls_interfaces']['mplsinterfaces']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['mplsinterfaces']['extract_mpls_interfaces']['mplsinterfaces']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="bgpvfourneighbors"
+					name="bgpvfourneighbors" value="extract_bgpv4_neighbour"
+					<?php if(in_array('extract_bgpv4_neighbour', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>IPV4 BGP Neighbors</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=bgpvfourneighbors&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['bgpvfourneighbors']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['bgpvfourneighbors']['neighbours'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="ran" name="ran" value="extract_show_ip_bgp_vpnv4_vrf_ran"
+					<?php if(in_array('extract_show_ip_bgp_vpnv4_vrf_ran', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>IPV4 BGP Routes</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=ran&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a> <?php
+    $color = ($output['ran']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['ran']['count'] . '</span>';
+    echo $display;
+    ?>
+</td>
+			</tr>
+			<tr>
+				<td><input type="checkbox" id="mplsneighbors" name="mplsneighbors"
+					value="extract_mpls_ldp_neighbour" <?php if(in_array('extract_mpls_ldp_neighbour', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>MPLS Neighbors</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=mplsneighbors&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    
+    $color = ($output['mplsneighbors']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['mplsneighbors']['message'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="bgpvsixneighbours"
+					name="bgpvsixneighbours" value="extract_bgp_v6_neighbour"
+					<?php if(in_array('extract_bgp_v6_neighbour', $_GET['category'])):?> checked="checked"
+					<?php endif;?>></td>
+				<td><b>IPV6 BGP Neighbors</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=bgpvsixneighbours&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a><?php
+    $color = ($output['bgpvsixneighbors']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['bgpvsixneighbours']['neighbours'] . '</span>';
+    echo $display;
+    ?>
+</td>
+				<td><input type="checkbox" id="bgpvsixroutes" name="bgpvsixroutes"
+					value="extract_bgpv6_routes" <?php if(in_array('extract_bgpv6_routes', $_GET['category'])):?>
+					checked="checked" <?php endif;?>></td>
+				<td><b>IPV6 BGP Routes</b></td>
+				<td><a id="anchorcmd" class="anchorcmd"
+					href="devdetmdl-cellsite.php?commandname=bgpvsixroutes&deviceid=<?php echo $_SESSION['deviceidswusr'];?>&deviceseries=<?php echo $_GET['deviceseries']?>&version=<?php echo $_GET['version']?>"><i
+						class="fa fa-file-text-o fa-lg text-primary"></i></a> <?php
+    $color = ($output['bgpvsixroutes']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . $output['bgpvsixroutes']['count'] . '</span>';
+    echo $display;
+    ?>
+</td>
+			</tr>
+			<tr>
+    <?php
+    if ($healthchktype != 'Load Table') {
+        $device_status = '<tr><td align="right" class="border-0"><b>Status: </b></td><td align="left" class="border-0"><b>';
+        // if($output['status']['error']){
+        if (($output['error']) || ($healthchktype == 'Custom' && count($_GET['category']) == 0)) {
+            $device_status .= 'Failed';
+        } else {
+            $device_status .= 'Reached';
+        }
+        $device_status .= '</b></td></tr>';
+    }
+    ?>
+    <td colspan="3" class="run_custom_checks text-center"
+					data-deviceid="<?php echo $deviceid ?>"
+					data-userid="<?php echo $_SESSION['userid'] ?>"
+					data-version="<?php echo $_GET['version'] ?>"
+					data-deviceseries="<?php echo $_GET['deviceseries'] ?>"
+					><button
+						class="btn btn-primary">Run selected health Checks</button></td>
+				<td colspan="3" class="run_all_checks text-center"
+					data-deviceid="<?php echo $deviceid ?>"
+					data-userid="<?php echo $_SESSION['userid'] ?>"
+					data-version="<?php echo $_GET['version'] ?>"
+					data-deviceseries="<?php echo $_GET['deviceseries'] ?>"
+					><button
+						class="btn btn-primary">Run All Health Checks</button></td>
+				<td colspan="3" class="text-center"><table class="border-0">
+						<tr>
+							<td align="right" class="float-right border-0"><b>Last Run On:</b></td>
+							<td align="left" class="border-0"><b><?php echo $lastupdated;?></b></td><?php echo $device_status; ?></tr>
+						<tr>
+							<td align="center" class="border-0" colspan="2">
+								<!-- <button type="button" id="but<?php echo $deviceid ?>" class="btn btn-sm auditLog" data-toggle="modal">Audit Log</button> -->
+							</td>
+						</tr>
+					</table></td>
+				<!-- <td colspan="2" class="run_preventive_checks" data-deviceid="<?php echo $deviceid ?>"  data-userid="<?php echo $_SESSION['userid'] ?>" style="cursor: pointer;">&nbsp;<!--<b>Run Preventive Health Checks</b></td> -->
+			</tr>
+		</tbody>
+	</table>
+</div>
---
+++ healthchk-cellsitetech-custom.php
@@ -0,0 +1,203 @@
+<script type="text/javascript">
+$(document).ready(function(){
+			$(document).on('click', '.anchorcmd', function(event) {
+            		var myModal = $('#mycmdModal');
+            		myModal.find('.modal-content').css('border', 'none'); 
+                    //myModal.find('.modal-content').html('<div id="ajax_loader" style="position: absolute; left: 50%; top: 50%; display: start;"><img src="resources/img/ajax-loader.gif"></img></div>');
+                    myModal.find('.modal-content').html('<div id="ajax_loader" style="position: absolute; left: 30%; top: 50%; display: start;">Loading... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>');
+                    myModal.modal('show'); 
+                	$.ajax({url: $(this).attr('href'), success: function(result){
+                		var myModal = $('#mycmdModal');
+                		myModal.find('.modal-content').css('border', '1px solid rgba(0, 0, 0, 0.2)');
+                        myModal.find('.modal-content').html(result);
+                        myModal.modal('show');
+                        
+                    }});
+                     $('#mycmdModal').removeData()   
+                     return false;                  
+              });
+            });        
+</script>
+<?php
+ini_set('display_errors', 1);
+include "classes/db2.class.php";
+include "classes/paginator.class.php";
+include 'functions.php';
+$userid = $_GET['userid'];
+$deviceid = $_GET['deviceid'];
+$lastupdated = date('Y-m-d H:i:s');
+if (count($_GET['category']) > 0) {
+    $category_imp = implode(',', $_GET['category']);
+    /* Custom HealthCheck API API Call */
+    $devicetype = 'ios';
+    $url_final = 'http://10.134.179.82:8085/healthcheck/custom/Cisco/'.$_GET['deviceseries'].'/'.$devicetype.'/'.$_GET['version'].'/'.$deviceid.'/'.$category_imp;
+    //$url_final = 'http:///njbboemsda3v.nss.vzwnet.com:8085/healthcheck/custom/Cisco/'.$_GET['deviceseries'].'/'.$devicetype.'/'.$_GET['version'].'/'.$deviceid.'/'.$category_imp;
+    // $url_final = 'http://njbboemsda3v.nss.vzwnet.com:8080/healthcheck/' . $devicetype . '/custom/' . $deviceid . '/' . $category_imp;
+    // $url_final = 'http://njbboemsda3v.nss.vzwnet.com:8080/healthcheck/'.$devicetype.'/custom/'.$deviceid.'/1,2,3';
+   $filename = 'customhealthcheck';
+     $msg = $url_final;
+   logToFile($filename, $msg);
+    $output = json_decode(sendPostData($url_final), true);
+    /*
+     * $output = '{
+     * "configregister": {
+     * "R": 0,
+     * "message": "configuration register = 0x2102"
+     * },
+     * "cpuutilization": {
+     * "R": 0,
+     * "message": "5s = 4%/1%; 1m = 3%; 5m= 3%"
+     * },
+     * "iosversion": {
+     * "R": 0,
+     * "message": "15.6(1)S1"
+     * }
+     * }';
+     */
+    // $output = json_decode($output, 1);
+}
+//print_r($output);
+$healthchktype = 'Custom';
+// $result = select_healthchk_info($deviceid);
+if (! in_array('extract_version_ios', $_GET['category']) || ! isset($output['iosversion'])) {
+    $output['iosversion'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_process_cpu', $_GET['category']) || ! isset($output['cpuutilization'])) {
+    $output['cpuutilization'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_memory_statistics', $_GET['category']) || ! isset($output['freememory'])) {
+    $output['freememory'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_buffers', $_GET['category']) || ! isset($output['buffers'])) {
+    $output['buffers'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_show_running_config', $_GET['category']) || ! isset($output['bootstatement'])) {
+    $output['bootstatement'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_show_platform', $_GET['category']) || ! isset($output['platform'])) {
+    $output['platform'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_alarms', $_GET['category']) || ! isset($output['environmental'])) {
+    $output['environmental'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_version_config', $_GET['category']) || ! isset($output['configregister'])) {
+    $output['configregister'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_count_interfaces', $_GET['category']) || ! isset($output['interfacecounters'])) {
+    $output['interfacecounters'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('twothsndbyteping', $_GET['category']) || ! isset($output['twothsndbyteping'])) {
+    $output['twothsndbyteping'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_bfd_neighbour', $_GET['category']) || ! isset($output['bfdsession'])) {
+    $output['bfdsession'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_show_logging', $_GET['category']) || ! isset($output['logentries'])) {
+    $output['logentries'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_show_vrf', $_GET['category']) || ! isset($output['vrfstates'])) {
+    $output['vrfstates'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_show_brief', $_GET['category']) || ! isset($output['interfacestates'])) {
+    $output['interfacestates'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_xconnect_all', $_GET['category']) || ! isset($output['xconnect'])) {
+    $output['xconnect'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_mlps_interfaces', $_GET['category']) || ! isset($output['mplsinterfaces'])) {
+    $output['mplsinterfaces'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+//if (! in_array('extract_bgpv4_neighbour', $_GET['category']) || ! isset($output['bgpvfourneighbors'])) {
+if (! in_array('extract_bgpv4_neighbour', $_GET['category']) || ! isset($output['bgpvfourneighbors'])) {
+    $output['bgpvfourneighbors'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_mlps_ldp_neighbour', $_GET['category']) || ! isset($output['mplsneighbors'])) {
+    $output['mplsneighbors'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array(11, $_GET['category']) || ! isset($output['configregister'])) {
+    $output['mplsneighbors'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_bgp_v6_neighbour', $_GET['category']) || ! isset($output['bgpvsixneighbours'])) {
+    $output['bgpvsixneighbours'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+
+if (! in_array('extract_show_ip_bgp_vpnv4_vrf_ran', $_GET['category']) || ! isset($output['ran'])) {
+    $output['ran'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+if (! in_array('extract_bgpv6_routes', $_GET['category']) || ! isset($output['bgpvsixroutes'])) {
+    $output['bgpvsixroutes'] = array(
+        'R' => '',
+        'message' => ''
+    );
+}
+// $output = '{"bfdsession":'.$result['bfdsession'].',"bgpvfourneighbors":'.$result['bgpvfourneighbors'].',"bgpvsixneighbours":'.$result['bgpvsixneighbours'].',"bgpvsixroutes":'.$result['bgpvsixroutes'].',"bootstatement":'.$result['bootstatement'].',"buffers":'.$result['buffers'].',"configregister":'.$result['configregister'].',"cpuutilization":'.$result['cpuutilization'].',"environmental":'.$result['environmental'].',"vrfstates":'.$result['vrfstates'].',"freememory":'.$result['freememory'].',"interfacecounters":'.$result['interfacecounters'].',"interfacestates":'.$result['interfacestates'].',"iosversion":'.$result['iosversion'].',"logentries":'.$result['logentries'].',"mplsinterfaces":'.$result['mplsinterfaces'].',"mplsneighbors":'.$result['mplsneighbors'].',"platform":'.$result['platform'].',"ran":'.$result['ran'].',"twothsndbyteping":'.$result['twothsndbyteping'].',"xconnect":'.$result['xconnect'].'}';
+// $output = json_decode($output,true);
+// print '<pre>';
+// print_r($output); die;
+update_healthchk_info($deviceid, $output, $lastupdated);
+$_SESSION['deviceidswusr'] = $deviceid;
+?>
+<?php include_once 'hc_blk_inc.php';?>

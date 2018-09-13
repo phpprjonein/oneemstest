a26ef619587f33b2e7a5422dd737d40f7c8c67a4
phpprjonein <phpprjonein@gmail.com>
2018-09-13 14:46:40 -0400

    unwantedfiles and last-release-13-sep-2018 zip file

--- api-test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if(rand(1,99) % 2 == 0){
-    $ret = true;
-}else{
-    $ret = false;
-}
-$myObject = array( 'result' => $ret ); // example
-echo json_encode($myObject); // outputs JSON text
-?>

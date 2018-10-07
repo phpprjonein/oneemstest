3b0dd6e90814c54ea2513df6fba22b98b925a6cd
phpprjonein <phpprjonein@gmail.com>
2018-10-07 08:30:49 -0400

    Modified files desg. API curl structure corrected ones

---
+++ devdetmdl-cellsite.php
@@ -0,0 +1,135 @@
+<?php
+$command = $_GET['commandname'];
+$deviceid = $_GET['deviceid'];
+$deviceseries = $_GET['deviceseries'];
+$version = $_GET['version'];
+// $deviceid = 2;
+
+switch ($command) {
+    
+    case 'freememory':
+        // $url='http://10.134.179.82:8080/healthcheck/ios/2///
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/8';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_memory_statistics';
+        break;
+    case 'buffers':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/5';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_buffers';
+        break;
+    // case 'showversion':
+    case 'iosversion':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/2';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_version_ios';
+        break;
+    case 'configregister':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/3';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_version_config';
+        break;
+    case 'environment':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/4';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_alarms';
+        break;
+    case 'platform':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/6';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_show_platform';
+        break;
+    case 'bfdsession':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/10';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bfd_neighbour';
+        break;
+    case 'interfacestates':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/9';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_show_brief';
+        break;
+    case 'showinterfaces':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/7';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_count_interfaces';
+        break;
+    case 'mplsinterfaces':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/12';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_mlps_interfaces';
+        break;
+    case 'mplsneighbors':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/11';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_mlps_ldp_neighbour';
+        break;
+    case 'bgpvsixroutes':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/17';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bgpv6_routes';
+        break;
+  /*  case 'show-running-config-|-i-boot':
+        $url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/16';
+        break;
+  */
+    case 'cpuutilization':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/1';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_process_cpu';
+        break;
+    case 'vrfstates':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/23';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_show_vrf';
+        break;
+    case 'twothsndbyteping':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/22';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_ping';
+        break;
+    case 'bgpvsixneighbours':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/14';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bgp_v6_neighbour';
+        break;
+    case 'bgpvfourneighbors':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/13';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bgpv4_neighbour';
+        break;
+    case 'xconnect':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/20';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_xconnect_all';
+        break;
+    // case 'bgpvfourroutes':
+    case 'ran':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/19';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_show_ip_bgp_vpnv4_vrf_ran';
+        break;
+    case 'bootstatement':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/16';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_show_running_config';
+        break;
+    case 'interfacecounters':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/7';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_count_interfaces';
+        break;
+    case 'logentries':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/15';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_show_logging';
+        break;
+    case 'bandwidth':
+        //$url = 'http://10.134.179.82:8080/healthcheck/ios/' . $deviceid . '/15';
+        $url = 'http://10.134.179.82:8080/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bandwidth';
+        break;
+    default:
+        break;
+}
+/* Ends */
+?>
+<div class="modal-header">
+	<h5 class="modal-title" id="exampleModalLabel">Device Console Output  - Command Issued - <?php echo $_GET['commandname'];?></h5>
+	<button type="button" class="close" data-dismiss="modal"
+		aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	</button>
+</div>
+
+<div class="modal-body">    
+<?php
+if (isset($url)) {
+    $data = @file_get_contents($url);
+} else
+    $data = "CLI command needs is in progress123." . $command . $device . $url;
+
+echo "<span style='font-family:courier;font-size:12px;font-weight:500;'> $data</span>";
+?>
+</div>
+<div class="modal-footer">
+	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+</div>
---
+++ instant_hc_blk_inc.php
@@ -0,0 +1,263 @@
+
+<div class="ownfont box-body launch-modal" id="health-chk-div-wrap">
+	<table class="table table-bordered" cellspacing="0" cellpadding="0">
+		<tbody>
+			<tr>
+				<td><b>IOS Version </b></td>
+				<td>
+<?php
+$color = ($output['iosversion']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['iosversion']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>CPU Utilization</b></td>
+				<td>
+<?php
+
+$color = ($output['cpuutilization']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['cpuutilization']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Free Memory</b></td>
+				<td>
+<?php
+
+$color = ($output['freememory']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['freememory']['message'] . '</span>';
+echo $display;
+?>
+</td>
+			</tr>
+			<tr>
+				<td><b>Buffers</b></td>
+				<td>
+<?php
+
+$color = ($output['buffers']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['buffers']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Boot Statement</b></td>
+				<td>
+<?php
+
+$color = ($output['bootstatement']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['bootstatement']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Platform </b></td>
+				<td>
+<?php
+
+$color = ($output['platform']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['platform']['message'] . '</span>';
+echo $display;
+?>
+</td>
+			</tr>
+
+			<tr>
+				<td><b>Environmental</b></td>
+				<td>
+<?php
+
+$color = ($output['environmental']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['environmental']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Config Register</b></td>
+				<td>
+<?php
+
+$color = ($output['configregister']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['configregister']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Interface Counters</b></td>
+				<td>
+<?php
+
+$color = ($output['interfacecounters']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['interfacecounters']['count'] . '</span>';
+echo $display;
+?>
+</td>
+			</tr>
+			<tr>
+				<td><b>2000 Byte Ping</b></td>
+				<td>
+<?php
+
+$color = ($output['twothsndbyteping']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['twothsndbyteping']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>BFD Sessions</b></td>
+				<td>
+<?php
+
+$color = ($output['bfdsession']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['bfdsession']['message'] . '</span>';
+echo $display;
+?>
+											  
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				<td><b>Log Entries</b></td>
+				<td>
+<?php
+
+if (isset($output['logentries']['buffer_logging']['R'])) {
+    $color = ($output['logentries']['buffer_logging']['R'] == 0) ? 'green' : 'red';
+    $display = "<span style='color:" . $color . "'>" . " Buffer Logging : " . $output['logentries']['buffer_logging']['value'] . '</span>';
+    echo $display;
+}
+?>
+											  <?php
+            
+            if (isset($output['logentries']['trap_logging']['R'])) {
+                $color = ($output['logentries']['trap_logging']['R'] == 0) ? 'green' : 'red';
+                $display = "<span style='color:" . $color . "'>" . "<br> Trap Logging : " . $output['logentries']['trap_logging']['value'] . '</span>';
+                echo $display;
+            }
+            ;
+            ?>
+											  </td>
+			</tr>
+			<tr>
+				<td><b>VRF</b></td>
+				<td>
+<?php
+
+$color = ($output['vrfstates']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['vrfstates']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Interface State</b></td>
+				<td>
+<?php
+
+$color = ($output['interfacestates']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['interfacestates']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>Xconnect</b></td>
+				<td>
+<?php
+
+$color = ($output['xconnect']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['xconnect']['count'] . '</span>';
+echo $display;
+?>
+</td>
+			</tr>
+			<tr>
+				<td><b>MPLS Interfaces</b></td>
+				<td>
+<?php
+
+$color = ($output['mplsinterfaces']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['mplsinterfaces']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>IPV4 BGP Neighbors</b></td>
+				<td>
+<?php
+
+$color = ($output['bgpvfourneighbors']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['bgpvfourneighbors']['neighbours'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>IPV4 BGP Routes</b></td>
+				<td>
+<?php
+$color = ($output['ran']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['ran']['count'] . '</span>';
+echo $display;
+?>
+</td>
+			</tr>
+			<tr>
+				<td><b>MPLS Neighbors</b></td>
+				<td>
+<?php
+
+$color = ($output['mplsneighbors']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['mplsneighbors']['message'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>IPV6 BGP Neighbors</b></td>
+				<td>
+<?php
+$color = ($output['bgpvsixneighbors']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['bgpvsixneighbours']['neighbours'] . '</span>';
+echo $display;
+?>
+</td>
+				<td><b>IPV6 BGP Routes</b></td>
+				<td>
+<?php
+$color = ($output['bgpvsixroutes']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['bgpvsixroutes']['count'] . '</span>';
+echo $display;
+?>
+</td>
+			</tr>
+ <tr>
+                                <td><b>Bandwidth</b></td>
+                                <td>
+<?php
+$color = ($output['bandwidth']['R'] == 0) ? 'green' : 'red';
+$display = "<span style='color:" . $color . "'>" . $output['bandwidth']['message'] . '</span>';
+echo $display;
+?>
+</td>
+</tr>
+			<tr>
+    <?php
+    if ($healthchktype != 'Load Table') {
+        $device_status = '<tr><td align="right" class="border-0"><b>Status: </b></td><td align="left" class="border-0"><b>';
+        // if($output['status']['error']){
+        if (($output['error']) || ($healthchktype == 'Custom' && count($_GET['category']) == 0)) {
+            $device_status .= 'Failed';
+        } else {
+            $device_status .= 'Reached';
+        }
+        $device_status .= '</b></td></tr>';
+    }
+    ?>
+    <!-- <td colspan="3" class="instant_run_all_checks text-center"  data-deviceid="<?php echo $deviceid ?>"  data-userid="<?php echo $_SESSION['userid'] ?>"><button class="btn btn-primary">Run All Health Checks</button></td>  -->
+				<td colspan="6" class="text-center"><table align="center"
+						class="border-0">
+						<tr>
+							<td align="right" class="float-right border-0"><b>Last Run On:</b></td>
+							<td align="left" class="border-0"><b><?php echo $lastupdated;?></b></td><?php echo $device_status; ?></tr>
+					</table></td>
+				<!-- <td colspan="2" class="run_preventive_checks" data-deviceid="<?php echo $deviceid ?>"  data-userid="<?php echo $_SESSION['userid'] ?>" style="cursor: pointer;">&nbsp;<!--<b>Run Preventive Health Checks</b></td> -->
+			</tr>
+		</tbody>
+	</table>
+</div>

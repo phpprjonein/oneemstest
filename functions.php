f5ca05416d17c7a077d44f2150f8c89f12055d20
phpprjonein <phpprjonein@gmail.com>
2018-12-06 23:04:53 +0530

    swinventory tab changed to swinventoryinterface

--- functions.php
+++ functions.php
@@ -5125,14 +5125,12 @@ function generate_option_button_for_configs_telco_interface($switchname, $type, 
         }else{
             $sql = "select interface,description from software_inventory where devicename = '".$devicename."' and description like '".$device_name."%'";
         }*/
-        
-        $tabcolname = ($type == 'even') ? 'telcointerfaceeven':'telcointerfaceodd';
-        //if($script_type != 'BW-Upgrade'){
+        if($script_type != 'BW-Upgrade'){
+            $sql = "SELECT distinct(interface), devicename FROM software_inventory where interface like 'TenGigE%' and devicename like '".$devicename."' and NOT interface LIKE '%.%' and interface != '' and interface != 'None' order by interface";
+        }else{
+            $tabcolname = ($type == 'even') ? 'telcointerfaceeven':'telcointerfaceodd';
             $sql = "SELECT distinct(".$tabcolname.") as interface, devicename FROM swinventoryinterface where ".$tabcolname." like 'TenGigE%' and devicename like '".$devicename."' and NOT ".$tabcolname." LIKE '%.%' and ".$tabcolname." != '' and ".$tabcolname." != 'None' order by ".$tabcolname;
-        //}else{
-        //    $sql = "select interface,description from software_inventory where devicename = '".$devicename."' and description like '".$device_name."%'";
-        //}
-        
+        }
         $db2->query($sql);
         $resultset = $db2->resultset();
         foreach ($resultset as $key => $val){

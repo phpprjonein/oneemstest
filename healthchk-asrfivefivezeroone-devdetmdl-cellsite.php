439585159fbc412d170209d8113568e7df1c4d98
phpprjonein <phpprjonein@gmail.com>
2019-01-25 23:45:18 +0530

    updated asr5501 files

--- healthchk-asrfivefivezeroone-devdetmdl-cellsite.php
+++ healthchk-asrfivefivezeroone-devdetmdl-cellsite.php
@@ -9,124 +9,59 @@ $version = $_GET['version'];
 
 switch ($command) {
     
-    case 'extract_redundancy':
+    case 'extract_version':
         // $url=$APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/2///
         //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/8';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_redundancy';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_version';
         break;
-    case 'extract_process_cpu':
+    case 'extract_processes_cpu':
         //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/5';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_process_cpu';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_processes_cpu';
+        break;
+    case 'extract_memory_summary':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/5';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_memory_summary';
         break;
     // case 'showversion':
-    case 'extract_memory_statistics':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/2';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_memory_statistics';
-        break;
-    case 'extract_version_rsp':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/3';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_version_rsp';
-        break;
-    case 'extract_version_installed_active':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/4';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_version_installed_active';
-        break;
-    case 'extract_version_installed_committed':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/6';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_version_installed_committed';
-        break;
-    case 'extract_alarms':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/10';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_alarms';
-        break;
-    case 'extract_power_supply':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/9';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_power_supply';
-        break;
     case 'extract_platform':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/7';
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/2';
         $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_platform';
         break;
-    case 'extract_fabric_status':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/12';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_fabric_status';
+    case 'extract_alarms_brief':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/3';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_alarms_brief';
         break;
-    case 'extract_fabric_inter_asic_drops':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/11';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_fabric_inter_asic_drops';
+    case 'extract_interfaces':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/4';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_interfaces';
         break;
-    case 'extract_fabric_inter_asic_errors':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/17';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_fabric_inter_asic_errors';
-        break;
-    case 'extract_fabric_inter_asic_link_status':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/1';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_fabric_inter_asic_link_status';
-        break;
-    case 'extract_up_down_status':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/23';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_up_down_status';
-        break;
-    case 'extract_count_interfaces':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/22';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_count_interfaces';
+    case 'extract_ping':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/6';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_ping';
         break;
     case 'extract_bfd_session':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/14';
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/10';
         $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bfd_session';
         break;
-    case 'extract_vrrp':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/13';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_vrrp';
+    case 'extract_logging':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/9';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_logging';
         break;
-    case 'extract_ntp':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/20';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_ntp';
-        break;
-    // case 'bgpvfourroutes':
-    case 'extract_global_ospf_neighbor':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/19';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_global_ospf_neighbor';
-        break;
-    case 'extract_1xrtt_ospf_neighbor':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/16';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_1xrtt_ospf_neighbor';
-        break;
-    case 'extract_cell_mgmt_ospf_neighbor':
+    case 'extract_vrf_all':
         //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/7';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_cell_mgmt_ospf_neighbor';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_vrf_all';
         break;
-    case 'extract_ospf_nsr_gr':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/15';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_ospf_nsr_gr';
+    case 'extract_ip_interface_brief':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/12';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_ip_interface_brief';
         break;
-    case 'extract_ldp_neighbor':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/15';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_ldp_neighbor';
+    case 'extract_mpls_interfaces':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/11';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_mpls_interfaces';
         break;
-    case 'extract_wdn_peering':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/19';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_wdn_peering';
-        break;
-    case 'extract_csr_peering':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/16';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_csr_peering';
-        break;
-    case 'extract_crs_wdn_peering':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/7';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_crs_wdn_peering';
-        break;
-    case 'extract_crs_lte_peering':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/15';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_crs_lte_peering';
-        break;
-    case 'extract_bgp_nsr_graceful_restart':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/15';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bgp_nsr_graceful_restart';
-        break;
-    case 'extract_air_filter_check':
-        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/15';
-        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_air_filter_check';
+    case 'extract_bgp_vpnv4_unicast_summary':
+        //$url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/ios/' . $deviceid . '/17';
+        $url = $APPCONFIG['healthcheck']['endpoint'].'/healthcheck/raw/Cisco/'.$deviceseries.'/ios/'.$version.'/'.$deviceid.'/extract_bgp_vpnv4_unicast_summary';
         break;
     default:
         break;

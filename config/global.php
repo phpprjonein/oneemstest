b6707b1f2dd9df46b9bc1722547c7bee4098bdfc
phpprjonein <phpprjonein@gmail.com>
2019-03-29 23:59:56 +0530

    Config giles are updated

--- config/global.php
+++ config/global.php
@@ -3,7 +3,11 @@
  * Application dynamic configuration loads
  */
 global $APPCONFIG;
-$APPCONFIG = parse_ini_file("config/appconfig.ini",true);
+if(file_exists('config/'.$_SERVER['SERVER_NAME'].'.ini')){
+    $APPCONFIG = parse_ini_file('config/'.$_SERVER['SERVER_NAME'].'.ini',true);
+}else{
+    $APPCONFIG = parse_ini_file("config/appconfig.ini",true);
+}
 if($_SERVER['SERVER_NAME'] == 'localhost'){
     $APPCONFIG = $APPCONFIG['localhost'];
 }else{

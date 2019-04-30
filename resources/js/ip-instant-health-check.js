48bba8300442dd09bb99b40828dae83e4e113b34
phpprjonein <phpprjonein@gmail.com>
2019-04-30 11:20:52 -0400

    Desg. changes 30-APR-2019

---
+++ resources/js/ip-instant-health-check.js
@@ -0,0 +1,347 @@
+$(document).ready(function() {
+  $(document).on(
+    "click",
+    ".instant-health-check #sync-password-ip-health-check",
+    function(event) {
+      var req_err = false;
+      $(".instant-health-check #status").html("");
+      $(".instant-health-check #status").css("opacity", "");
+      if (
+        $(".instant-health-check #syncPassInputDeviceIPaddress").val() == ""
+      ) {
+        $(".instant-health-check #status").html(
+          "<strong>Error!</strong> Sync Password field is required.<br/>"
+        );
+        $(".instant-health-check #status").addClass("alert-danger");
+        $(".instant-health-check #status").show();
+        req_err = true;
+      }
+      if (req_err) {
+        window.setTimeout(function() {
+          $(".alert")
+            .fadeTo(500, 0)
+            .slideUp(500, function() {
+              $(this).hide();
+            });
+        }, 4000);
+        return false;
+      }
+      var myModal = $("#Modal_Health_Checks");
+      $("#Modal_Health_Checks .modal-title").html("Sync Password");
+      $("#Modal_Health_Checks .modal-body").html(
+        '<div class="text-center overlay box-body">Please wait... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>'
+      );
+      myModal.modal("show");
+      $.ajax({
+        type: "post",
+        url: "ip-mgt-process.php",
+        data: {
+          ctype: "Sync Password",
+          syncpass_ipaddress: $("#syncPassInputDeviceIPaddress").val()
+        },
+        success: function(resdata) {
+          $("#Modal_Health_Checks .modal-body").html(resdata);
+          return false;
+        }
+      });
+      return false;
+    }
+  );
+  $(document).on(
+    "click",
+    ".instant-health-check #sync-cyberark-sync-password-ip-health-check",
+    function(event) {
+      var req_err = false;
+      $(".instant-health-check #status").html("");
+      $(".instant-health-check #status").css("opacity", "");
+      if ($(".instant-health-check #synccyberarkpass_ipaddress").val() == "") {
+        $(".instant-health-check #status").html(
+          "<strong>Error!</strong> Check CyberArk Synchronization field is required.<br/>"
+        );
+        $(".instant-health-check #status").addClass("alert-danger");
+        $(".instant-health-check #status").show();
+        req_err = true;
+      }
+      if (req_err) {
+        window.setTimeout(function() {
+          $(".alert")
+            .fadeTo(500, 0)
+            .slideUp(500, function() {
+              $(this).hide();
+            });
+        }, 4000);
+        return false;
+      }
+      var myModal = $("#Modal_Health_Checks");
+      $("#Modal_Health_Checks .modal-title").html(
+        "Check CyberArk Synchronization Status"
+      );
+      $("#Modal_Health_Checks .modal-body").html(
+        '<div class="text-center overlay box-body">Please wait... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>'
+      );
+      myModal.modal("show");
+      $.ajax({
+        type: "post",
+        url: "ip-mgt-process.php",
+        data: {
+          ctype: "SyncCyberArk",
+          syncpass_ipaddress: $("#synccyberarkpass_ipaddress").val()
+        },
+        success: function(resdata) {
+          $("#Modal_Health_Checks .modal-body").html(resdata);
+          return false;
+        }
+      });
+      return false;
+    }
+  );
+  $(document).on("click", ".instant-health-check #cyberark-add-acct", function(
+    event
+  ) {
+    var req_err = false;
+    $(".instant-health-check #status").html("");
+    $(".instant-health-check #status").css("opacity", "");
+    if ($(".instant-health-check #cyberarkaddacct_ipaddress").val() == "") {
+      $(".instant-health-check #status").html(
+        "<strong>Error!</strong> IPV6 address for adding Cyber Ark account is required.<br/>"
+      );
+      $(".instant-health-check #status").addClass("alert-danger");
+      $(".instant-health-check #status").show();
+      req_err = true;
+    }
+    if (req_err) {
+      window.setTimeout(function() {
+        $(".alert")
+          .fadeTo(500, 0)
+          .slideUp(500, function() {
+            $(this).hide();
+          });
+      }, 4000);
+      return false;
+    }
+    var myModal = $("#Modal_Health_Checks");
+    $("#Modal_Health_Checks .modal-title").html("Adding CyberArk Account");
+    $("#Modal_Health_Checks .modal-body").html(
+      '<div class="text-center overlay box-body">Please wait... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>'
+    );
+    myModal.modal("show");
+    //setTimeout(function(){
+    $.ajax({
+      type: "post",
+      url: "ip-mgt-process.php",
+      data: {
+        ctype: "CyberArkadd",
+        syncpass_ipaddress: $("#cyberarkaddacct_ipaddress").val()
+      },
+      success: function(resdata) {
+        $("#Modal_Health_Checks .modal-body").html(resdata);
+        return false;
+      }
+    });
+    //}, 5000);
+    return false;
+  });
+
+  $(document).on(
+    "click",
+    ".instant-health-check #ssh-PuTTY-ip-health-check",
+    function(event) {
+      var req_err = false;
+      $(".instant-health-check #status").html("");
+      $(".instant-health-check #status").css("opacity", "");
+      if ($(".instant-health-check #sshPuTTYInputIPv6").val() == "") {
+        $(".instant-health-check #status").html(
+          "<strong>Error!</strong> Show PuTTY CmdLine field is required.<br/>"
+        );
+        $(".instant-health-check #status").addClass("alert-danger");
+        $(".instant-health-check #status").show();
+        req_err = true;
+      }
+      if (req_err) {
+        window.setTimeout(function() {
+          $(".alert")
+            .fadeTo(500, 0)
+            .slideUp(500, function() {
+              $(this).hide();
+            });
+        }, 4000);
+        return false;
+      }
+      var myModal = $("#Modal_Health_Checks");
+      $("#Modal_Health_Checks .modal-title").html("Show Putty Command Line");
+      var sshPuTTYInputIPv6 = $(".instant-health-check #sshPuTTYInputIPv6")
+        .val()
+        .replace(/\:/g, "-");
+      var command =
+        $("#username").val() +
+        "@PAMadmingrp@" +
+        sshPuTTYInputIPv6 +
+        "@pamssh.nsiam.vzwnet.com";
+
+      // Afotey add
+      var command2 =
+        $("#username").val() +
+        "@PAMronlygrp@" +
+        sshPuTTYInputIPv6 +
+        "@pamssh.nsiam.vzwnet.com";
+
+      $("#Modal_Health_Checks .modal-body").html(
+        '<div class="input-group mb-3"><input type="text" class="form-control" size="100" name="textbox" id="textboxp1" readonly value="' +
+          command +
+          '" >&nbsp;<button class="btn btn-default"  onclick="copyToClipboard(\'textboxp1\')">Copy</button></div>'
+      );
+
+      //   Afotey add
+      $("#Modal_Health_Checks .modal-body").append(
+        '<div class="input-group mb-3"><input type="text" class="form-control" size="100" name="textbox" id="textboxp2" readonly value="' +
+          command2 +
+          '" >&nbsp;<button class="btn btn-default" onclick="copyToClipboard(\'textboxp2\')">Copy</button></div>'
+      );
+
+      myModal.modal("show");
+      return false;
+    }
+  );
+
+  $(document).on("click", "#sshcmdoutputcpy", function(event) {
+    var copyText = document.getElementById("sshcmdoutput");
+    copyText.select();
+    document.execCommand("copy");
+    // alert("Copied the text: " + copyText.value);
+  });
+  $("#slectchktype input[type=radio][name=optradio]").change(function() {
+    if (this.value == "ipaddress") {
+      $("#inputDeviceName").hide();
+      $("#inputDeviceIPaddress").show();
+      $("#inputDeviceName").val("");
+    } else {
+      $("#inputDeviceIPaddress").hide();
+      $("#inputDeviceName").show();
+      $("#inputDeviceIPaddress").val("");
+    }
+  });
+  $(document).on("click", "#ip-health-check", function(event) {
+    var req_err = false;
+    //alert( 'backupsubmit clicked');
+    $("#instant-health-check #status").html("");
+    $("#instant-health-check #status").css("opacity", "");
+
+    if (
+      $("#instant-health-check #inputDeviceIPaddress").val() == "" &&
+      $("#instant-health-check #inputDeviceName").val() == ""
+    ) {
+      $("#instant-health-check #status").append(
+        "<strong>Error!</strong> IP Address/Device Name field is required.<br/>"
+      );
+      $("#instant-health-check #status").addClass("alert-danger");
+      req_err = true;
+      if (req_err) {
+        $("#instant-health-check .panel-body-wrap").html("");
+        $("#instant-health-check #status").show();
+        window.setTimeout(function() {
+          $(".alert")
+            .fadeTo(500, 0)
+            .slideUp(500, function() {
+              $(this).hide();
+            });
+        }, 4000);
+        return false;
+      }
+    }
+
+    if ($("#instant-health-check #inputDeviceIPaddress").val() != "") {
+      $.post("ip-mgt-process.php", {
+        calltype: "trigger",
+        action: "HC-IP-Validate-Disc",
+        type: "IPv4ORIPv6",
+        ipaddress: $("#instant-health-check #inputDeviceIPaddress").val()
+      }).done(function(data) {
+        if (data != "success") {
+          $("#instant-health-check #status").append(
+            "<strong>Error!</strong> Device IP Address is not valid.<br/>"
+          );
+          $("#instant-health-check #status").addClass("alert-danger");
+          req_err = true;
+          if (req_err) {
+            $("#instant-health-check .panel-body-wrap").html("");
+            $("#instant-health-check #status").show();
+            window.setTimeout(function() {
+              $(".alert")
+                .fadeTo(500, 0)
+                .slideUp(500, function() {
+                  $(this).hide();
+                });
+            }, 4000);
+            return false;
+          }
+        } else {
+          var ajs = $.ajax({
+            type: "get",
+            url: "deviceip-healthchk-instant-check-data.php",
+            data: {
+              deviceip: $("#inputDeviceIPaddress").val(),
+              userid: $("#userid").val()
+            },
+            beforeSend: function() {
+              $("#instant-health-check .panel-body-wrap").html(
+                '<div class="text-center overlay box-body">Running Health Checks. Takes several minutes... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>'
+              );
+            },
+            success: function(resdata) {
+              $("#instant-health-check .panel-body-wrap").html(resdata);
+              var table = $("#example").DataTable({
+                paging: false,
+                searching: false,
+                ordering: false,
+                info: false,
+                oLanguage: {
+                  sEmptyTable: "Device does not exist"
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+
+    if ($("#instant-health-check #inputDeviceName").val() != "") {
+      var ajs = $.ajax({
+        type: "get",
+        url: "deviceip-healthchk-instant-check-data.php",
+        data: {
+          devicename: $("#inputDeviceName").val(),
+          userid: $("#userid").val()
+        },
+        beforeSend: function() {
+          $("#instant-health-check .panel-body-wrap").html(
+            '<div class="text-center overlay box-body">Running Health Checks. Takes several minutes... <div class="fa fa-refresh fa-spin" style="font-size:24px; text-align:center;"></div></div>'
+          );
+        },
+        success: function(resdata) {
+          $("#instant-health-check .panel-body-wrap").html(resdata);
+          var table = $("#example").DataTable({
+            paging: false,
+            searching: false,
+            ordering: false,
+            info: false,
+            oLanguage: {
+              sEmptyTable: "Device does not exist"
+            }
+          });
+        }
+      });
+    }
+    return false;
+  });
+});
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("textboxp1");
+  /* Select the text field */
+  copyText.select();
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}

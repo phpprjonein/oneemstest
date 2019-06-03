c70a5ab4a4f865a4d1ffcc0be7b313f68979c94c
phpprjonein <phpprjonein@gmail.com>
2019-06-02 20:31:44 -0400

    Desg.changes

---
+++ help-nav.php
@@ -0,0 +1,60 @@
+<nav id="navbar-help" class="navbar navbar-light bg-light">
+								<nav class="nav nav-pills flex-column">
+									<h6 class="text-center">CONTENTS</h6>
+									<a class="nav-link" href="help.php">GETTING STARTED</a>
+									<a class="nav-link" href="help_network_elements.php">NETWORK ELEMENTS</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_network_elements.php#item-2-2-1">List Management Options</a>
+										<a class="nav-link ml-3 my-1" href="help_network_elements.php#item-2-3">Health Check Details View</a>
+									</nav>
+									<a class="nav-link" href="help_backup.php">BACKUP</a>
+									<a class="nav-link" href="help_config.php">CONFIGURATION</a>
+										<nav class="nav nav-pills flex-column">
+											<a class="nav-link ml-3 my-1" href="help_config.php#item-4-1">Load Template - Modification</a>
+											<a class="nav-link ml-3 my-1" href="help_config.php#item-4-2">Generate Script -  Modification</a>
+											<a class="nav-link ml-3 my-1" href="help_config.php#item-4-2-1">Generate Script - Golden</a>
+											<a class="nav-link ml-3 my-1" href="help_config.php#item-4-2-2">Template - View / Modify</a>
+											<a class="nav-link ml-3 my-1" href="help_config.php#item-4-3">Batch Tracking</a>
+										</nav>
+									<a class="nav-link" href="help_discovery_ips.php">DISCOVERY IPs</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_discovery_ips.php#item-5-1">Subnet Addition</a>
+									</nav>
+									<a class="nav-link" href="help_discovery_results.php">DISCOVERY RESULTS</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_discovery_results.php#item-6-1">Missed IP Addresses</a>
+										<a class="nav-link ml-3 my-1" href="help_discovery_results.php#item-6-2">New IP Addresses</a>
+										<a class="nav-link ml-3 my-1" href="help_discovery_results.php#item-6-3">OK IP Addresses</a>
+										<a class="nav-link ml-3 my-1" href="help_discovery_results.php#item-6-4">Manual Discovery</a>
+									</nav>
+									<a class="nav-link" href="help_maintenance.php">MAINTENANCE</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_maintenance.php#item-7-1">Software Delivery</a>
+											<a class="nav-link ml-3 my-1" href="help_maintenance.php#item-7-2">Reboot</a>
+											<a class="nav-link ml-3 my-1" href="help_maintenance.php#item-7-3">Boot Order Sequence</a>
+                                    </nav>
+									<a class="nav-link" href="help_tools.php">TOOLS</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_tools.php#item-8-1">Run All Health Checks</a>
+									</nav>
+									<a class="nav-link" href="help_audit.php">AUDIT</a>
+									<nav class="nav nav-pills flex-column">
+                                        <a class="nav-link ml-3 my-1" href="help_audit.php#item-audit-1">Customized Audit</a>
+                                        <a class="nav-link ml-3 my-1" href="help_audit.php#item-audit-2">Customized Audit History</a>
+									</nav>
+									<a class="nav-link" href="help_inventory.php">INVENTORY</a>
+									<a class="nav-link" href="help_admin.php">ADMIN</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_admin.php#item-9-1">Load Template - Golden</a>
+										<a class="nav-link ml-3 my-1" href="help_admin.php#item-9-2">Maintenance</a>
+										<a class="nav-link ml-3 my-1" href="help_admin.php#item-9-3">Device Cleanup</a>
+                                    </nav>
+									<a class="nav-link" href="help_ssh.php">PAM SSH USER GUIDE</a>
+									<nav class="nav nav-pills flex-column">
+										<a class="nav-link ml-3 my-1" href="help_ssh.php#item-ssh2">Command Line Usage - SSH</a>
+										<a class="nav-link ml-3 my-1" href="help_ssh.php#item-ssh3">SSH Application Usage: PuTTY</a>
+										<a class="nav-link ml-3 my-1" href="help_ssh.php#item-ssh4">SSH Application Usage: SecureCRT</a>
+                                    </nav>
+									<a class="nav-link" href="help_faqs.php">FAQs</a>
+								</nav>
+							</nav>

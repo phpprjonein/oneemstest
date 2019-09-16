e3b619c3e52ca743842cd35e5f9ad3dc69f1a0c5
phpprjonein <phpprjonein@gmail.com>
2019-09-15 20:10:41 -0400

    modified files

---
+++ logout.php
@@ -0,0 +1,79 @@
+<?php
+include "classes/db2.class.php";
+include "classes/paginator.class.php";
+include 'functions.php';
+
+// Static variable values set
+if (isset($_GET['clear'])) {
+    if (strtolower($_GET['clear']) == 'search') {
+        unset($_SESSION['search_term']);
+    }
+}
+
+user_session_check();
+// check_user_authentication('1'); //cellsite tech type user
+$page_title = 'OneEMS';
+/*
+ * $userleveldesc = get_user_level_desc($_SESSION['userlevel']);
+ * $username = $_SESSION['username'];
+ * $logmesgdesccode = logmessages_codes('C002');
+ * $mesg = " User name: $username | User type : $userleveldesc | Log Message Code : 'C002' | Message : $logmesgdesccode";
+ * write_log($mesg);
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes.php");  ?>
+	<script src="resources/js/login_impersonate.js?t="
+	<?php echo date('his'); ?>></script>
+</head>
+<body class="hold-transition skin-blue sidebar-mini ownfont">
+	<!-- Modal HTML -->
+	<div class="container-fluid">
+            <?php include ('menu.php'); ?>
+        <?php
+        $values = array(
+            'Logout' => '#'
+        );
+        echo generate_site_breadcrumb($values);
+        ?>
+                    <div class="col-auto">
+						<?php
+    if (isset($_SESSION['userlevel']) && $_SESSION['userlevel']) {
+        $location_href = get_landing_page();
+    } else {
+        session_destroy();
+        header('Location:index.php');
+        exit();
+    }
+    ?>
+
+
+<!-- Logut page content row main -->
+			<div class="row logout text-center">
+				<div
+					class="col-sm-2 col-2 logout-left d-flex justify-content-center align-items-center">
+					<i class="fa fa-sign-out fa-3x"></i>
+				</div>
+				<div class="col-sm-10 col-10">
+                               <?php  if ($_SESSION['sso_flag'] == 0) { ?>
+					<a href="index.php"><h5>CLICK HERE TO LOG OUT</h5></a>
+				<?php } else { ?>
+ <a href="https://logindev.ebiz.verizon.com/onesso/login.jsp?TYPE=33554433&REALMOID=06-12035133-bb2a-1020-90b6-85114dc6fbc3&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=$SM$Obk1gdJc3jGvdncG6BVVdHZ%2fQ0Ex627Y2pqBng6CDqNJdPLkjB768sxKLQTIhg%2b0&TARGET=$SM$HTTPS%3a%2f%2foneems-uat%2evh%2evzwnet%2ecom%2f"><h5>CLICK HERE TO LOG OUT</h5></a> 
+                                  <?php }; ?>
+					<p class="font-weight-bold">Didn't mean to log out?</p>
+					<p>
+						<a href="<?php echo $location_href;?>"><i
+							class="fa fa-sign-in fa-lg text-primary"></i> HOME</a>
+					</p>
+				</div>
+			</div>
+			<!-- /Loogut page content row main -->
+
+		</div>
+	</div>
+	<?php include ('footer.php'); ?>
+    </body>
+</html>

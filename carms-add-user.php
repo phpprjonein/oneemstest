93f20c86860f033c5ff5a3263a58bdd17d19e33d
phpprjonein <phpprjonein@gmail.com>
2019-10-11 16:17:57 -0400

    SSO valid user but invalid oneems user page

--- carms-add-user.php
+++ carms-add-user.php
@@ -9,7 +9,7 @@
 			<div class="nav">
 				<div class="pull-left box">
 					<a class="navbar-brand" href="#"> <img
-						src="resources/img/ncmlogo.png" height="24px" alt="NCM Logo" />
+						src="resources/img/verizonlogo.png" height="24px" alt="Verizon Logo" />
 					</a>
 				</div>
 			</div>
@@ -30,10 +30,18 @@
 				</div>
 				<div class="col-sm-10 col-10">
                                <?php  if ($_SESSION['sso_flag'] == 0) { ?>
-					<a href="index.php"><h5>User not exist! Please add in Carms API</h5></a>
+					<!--<a href="index.php"><h5>User name not Found! Please get registered in CARMS </h5></a>-->
+					<h5>User not Found! Please register through CARMS </h5>
 				<?php }?>
 					<p>
-						<a href="index.php"><i
+ <!--
+<a href="https://logindev.ebiz.verizon.com/onesso/login.jsp?TYPE=33554433&REALMOID=06-12035133-bb2a-1020-90b6-85114dc6fbc3&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=$SM$Obk1gdJc3jGvdncG6BVVdHZ%2fQ0Ex627Y2pqBng6CDqNJdPLkjB768sxKLQTIhg%2b0&TARGET=$SM$HTTPS%3a%2f%2foneems-uat%2evh%2evzwnet%2ecom%2f"><h5>CLICK HERE TO LOG OUT</h5></a>
+-->
+<!--
+<a href="https://login.verizon.com/onesso/login.jsp?TYPE=33554433&REALMOID=06-12035133-bb2a-1020-90b6-85114dc6fbc3&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=$SM$uonu4YrCLDv6EqF6yb59tr2UPYs4S8GBgbQOF6U2p7vQHpMU10uLjWXD%2bg4bQRLc&TARGET=$SM$HTTPS%3a%2f%2foneems%2evh%2evzwnet%2ecom%2f"><h5>CLICK HERE TO LOG OUT</h5></a>
+-->
+<a href="https://login.verizon.com/onesso/login.jsp?TYPE=33554433&REALMOID=06-12035133-bb2a-1020-90b6-85114dc6fbc3&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=$SM$uonu4YrCLDv6EqF6yb59tr2UPYs4S8GBgbQOF6U2p7vQHpMU10uLjWXD%2bg4bQRLc&TARGET=$SM$HTTPS%3a%2f%2foneems%2evh%2evzwnet%2ecom%2f">
+<i
 							class="fa fa-sign-in fa-lg text-primary"></i> Login</a>
 					</p>
 				</div>

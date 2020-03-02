ba19410b9390330e784cb52f80f301f6c63a99d3
phpprjonein <phpprjonein@gmail.com>
2020-03-02 23:44:30 +0530

    config file updates

--- oneemstest/functions.php
+++ oneemstest/functions.php
@@ -22,30 +22,32 @@ function logToFile($filename, $msg)
 function onerr(){
     ini_set('display_errors',1);
 }
-
-/**
- *
- * @param unknown $username
- * @param unknown $password
- * @return unknown|boolean
- */
+function httpPost($endpoint,$params)
+{
+    global $APPCONFIG;
+    $url = $APPCONFIG['api']['domain'].'/'.$endpoint; 
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($ch, CURLOPT_USERPWD, $APPCONFIG['api']['username'].':'.$APPCONFIG['api']['password']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output=curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
 function get_user_info($username, $password)
 {
-    global $db2;
-    
-    if (trim($username) != '' && trim($password) != '') {
-        $password = md5($password);
-        $sql = "SELECT u.*,ul.userlevel as role FROM users u, userlevels ul WHERE u.username='" . $username . "' AND 
-        u.password='" . $password . "' AND ul.id = u.userlevel AND u.status = 1";
-        $db2->query($sql);
-        $rows = $db2->resultset();
-        $result = $rows[0];
-        
-        return $result;
-    }
-    return false;
+    $data_array =  array(
+            "action"          => 'login',
+            "function"          => 'get_user_info',
+            "username"        => trim($username),
+            "password"        => trim($password),
+    );
+    $result = httpPost('login.php', $data_array);
+    return json_decode($result, true);
 }
-
 /**
  *
  * @param unknown $username
@@ -319,85 +321,13 @@ function get_device_list_from_nodes($user_id)
  */
 function get_device_list_from_nodes_datatable($userid)
 {
-    global $db2, $pages;
-    
-    // print_r($_GET);
-    $draw = $_GET['draw'];
-    $start = isset($_GET['start']) ? $_GET['start'] : 0;
-    $length = isset($_GET['length']) ? $_GET['length'] : 10;
-    $search = trim($_GET['search']['value']) ? addslashes(trim($_GET['search']['value'])) : null;
-    $order_col = $_GET['order'][0]['column'];
-    $order_dir = $_GET['order'][0]['dir'];
-    
-    $columns = array(
-        'distinct(n.id)',
-        'n.devicename',
-        'n.csr_site_id',
-        'n.csr_site_name',
-        'n.region',
-        'n.market',
-        'n.deviceseries',
-        'n.nodeVersion'
+    $data_array =  array(
+            "action"          => 'backup',
+            "function"        => 'get_device_list_from_nodes_datatable',
+            "userid"          => $userid
     );
-    
-    $sql_count = "SELECT COUNT(DISTINCT(n.id)) ";
-    $sql_select = "SELECT " . implode(", ", $columns);
-    
-    $sql_condition = " FROM userdevices ud
-       JOIN nodes n on ud.nodeid = n.id
-       WHERE ud.userid = " . $userid . " AND status = 3";
-    if ($search) {
-        $sql_condition .= " AND ( ";
-        $sql_condition .= " n.devicename LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.csr_site_id  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.csr_site_name  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.market  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.deviceseries  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.nodeVersion  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.lastpolled  LIKE '%" . $search . "%'";
-        $sql_condition .= " ) ";
-    }
-    $count_sql = $sql_count . $sql_condition;
-    // echo $count_sql; die;
-    $db2->query($count_sql);
-    $row = $db2->resultsetCols();
-    
-    $total_rec = $row[0];
-    
-    $sql_order = "";
-    if ($order_col != '') {
-        $sql_order = " ORDER BY " . $columns[$order_col];
-    }
-    
-    if ($order_dir != '') {
-        $sql_order .= $order_dir != '' ? " $order_dir " : " asc ";
-    }
-    
-    $sql_limit = " LIMIT $start, $length ";
-    
-    $sql = $sql_select . $sql_condition . $sql_order . $sql_limit;
-    // echo '<br>';
-    // echo $sql;
-    
-    $db2->query($sql);
-    
-    $resultset['draw'] = $draw;
-    
-    if ($db2->resultset()) {
-        foreach ($db2->resultset() as $key => $value) {
-            $value['DT_RowId'] = "row_" . $value['id'];
-            $records[$key] = $value;
-        }
-        $resultset['data'] = $records;
-        $resultset['recordsTotal'] = $total_rec;
-        $resultset['recordsFiltered'] = $total_rec;
-    } else {
-        $resultset['data'] = array();
-        $resultset['recordsTotal'] = 10;
-        $resultset['recordsFiltered'] = 0;
-    }
-    
-    return $resultset;
+    $result = httpPost('backup.php', $data_array);
+    return json_decode($result, true);
 }
 
 function get_device_list_for_user($userid, $listid){
@@ -3524,9 +3454,6 @@ function get_device_list_from_backuprestore_datatable_export($userid, $listname 
  */
 function get_device_list_from_backuprestore_datatable($userid, $listname = '')
 {
-    global $db2, $pages;
-    
-    // print_r($_GET);
     $draw = $_GET['draw'];
     $start = isset($_GET['start']) ? $_GET['start'] : 0;
     $length = isset($_GET['length']) ? $_GET['length'] : 10;
@@ -3534,79 +3461,14 @@ function get_device_list_from_backuprestore_datatable($userid, $listname = '')
     $order_col = $_GET['order'][0]['column'];
     $order_dir = $_GET['order'][0]['dir'];
     
-    $columns = array(
-            'distinct(n.id)',
-            'n.devicename',
-            'n.csr_site_id',
-            'n.csr_site_name',
-            'n.region',
-            'n.market',
-            'n.deviceseries',
-            'n.nodeVersion'
+    $data_array =  array(
+            "action"          => 'backup',
+            "function"        => 'get_device_list_from_backuprestore_datatable',
+            "userid"          => $userid,
+            "listname"        => $listname
     );
-    $sql_count = "SELECT COUNT(distinct(n.id)) ";
-    $sql_select = "SELECT " . implode(", ", $columns);
-    
-    $sql_condition = " FROM userdevices ud
-       JOIN nodes n on ud.nodeid = n.id
-       WHERE ud.userid = " . $userid;
-    
-    if ($listname != '') {
-        $sql_condition .= " AND(ud.listname = '" . $listname . "')";
-    }
-    // die;
-    
-    if ($search) {
-        $sql_condition .= " AND ( ";
-        $sql_condition .= " n.devicename LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.csr_site_id  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.csr_site_name  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.market  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.deviceseries  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.nodeVersion  LIKE '%" . $search . "%'";
-        $sql_condition .= " OR n.lastpolled  LIKE '%" . $search . "%'";
-        $sql_condition .= " ) ";
-    }
-    $count_sql = $sql_count . $sql_condition;
-    // echo $count_sql;
-    $db2->query($count_sql);
-    $row = $db2->resultsetCols();
-    
-    $total_rec = $row[0];
-    
-    $sql_order = "";
-    if ($order_col != '') {
-        $sql_order = " ORDER BY " . $columns[$order_col];
-    }
-    
-    if ($order_dir != '') {
-        $sql_order .= $order_dir != '' ? " $order_dir " : " asc ";
-    }
-    
-    $sql_limit = " LIMIT $start, $length ";
-    
-    $sql = $sql_select . $sql_condition . $sql_order . $sql_limit;
-    // echo '<br>';
-    // echo $sql;
-    
-    $db2->query($sql);
-    
-    $resultset['draw'] = $draw;
-    
-    if ($db2->resultset()) {
-        foreach ($db2->resultset() as $key => $value) {
-            $value['DT_RowId'] = "row_" . $value['id'];
-            $records[$key] = $value;
-        }
-        $resultset['data'] = $records;
-        $resultset['recordsTotal'] = $total_rec;
-        $resultset['recordsFiltered'] = $total_rec;
-    } else {
-        $resultset['data'] = array();
-        $resultset['recordsTotal'] = 10;
-        $resultset['recordsFiltered'] = 0;
-    }
-    return $resultset;
+    $result = httpPost('backup.php?'.http_build_query($_GET), $data_array);
+    return json_decode($result, true);
 }
 
 
@@ -3617,11 +3479,20 @@ function get_device_list_from_backuprestore_datatable($userid, $listname = '')
  */
 function get_celltechusers_list($userid)
 {
+    $data_array =  array(
+            "action"          => 'backup',
+            "function"        => 'get_celltechusers_list',
+            "userid"          => $userid
+    );
+    $result = httpPost('backup.php', $data_array);
+    return json_decode($result, true);
+    /*
     global $db2;
     $sql = "select distinct(listid), listname from userdevices where userid=" . $userid;
     $db2->query($sql);
     $resultset['result'] = $db2->resultset();
     return $resultset['result'];
+    */
 }
 
 /**
